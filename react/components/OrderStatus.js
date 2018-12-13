4b4b42c53689de8d269b2ff3379a6188c9b6b486
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-13 19:14:16 -0200

    Add static OrderStatus component

---
+++ react/components/OrderStatus.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Tag } from 'vtex.styleguide'
+
+const OrderStatus = () => {
+  return (
+    <Tag type="warning">
+      Status do pedido (query)
+    </Tag>
+  )
+}
+
+export default OrderStatus
