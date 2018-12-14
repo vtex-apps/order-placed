83facc6126bf28c335baa2cc275c9c8819605f49
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-14 15:15:11 -0200

    Update import paths and render OrderInfo component

--- react/OrderPlaced.js
+++ react/OrderPlaced.js
@@ -1,37 +1,40 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { compose, graphql } from 'react-apollo'
+// import PropTypes from 'prop-types'
+// import { compose, graphql } from 'react-apollo'
 
-import getOrderGroup from './graphql/getOrderGroup.graphql'
-import Header from './components/Header'
+// import getOrderGroup from './graphql/getOrderGroup.graphql'
+import Header from './components/Header/Header'
+import OrderInfo from './components/OrderInfo/OrderInfo'
 import './styles.global.css'
 
 class OrderPlaced extends Component {
   render() {
     return (
-      <div className="w-70 center">
+      <div className="page-container">
         <Header />
+        <hr className="bg-muted-5 w-95" size="1" />
+        <OrderInfo />
       </div>
     )
   }
 }
 
-OrderPlaced.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+// OrderPlaced.propTypes = {
+//   data: PropTypes.object.isRequired,
+// }
 
-export default compose(
-  OrderPlaced,
-  graphql(getOrderGroup, {
-    options({ params }) {
-      return {
-        errorPolicy: params.orderGroup ? 'all' : 'none',
-        variables: {
-          orderGroup: params.orderGroup,
-        },
-      }
-    },
-  }),
-)(OrderPlaced)
+// export default compose(
+//   OrderPlaced,
+//   graphql(getOrderGroup, {
+//     options({ params }) {
+//       return {
+//         errorPolicy: params.orderGroup ? 'all' : 'none',
+//         variables: {
+//           orderGroup: params.orderGroup,
+//         },
+//       }
+//     },
+//   }),
+// )(OrderPlaced)
 
-// export default OrderPlaced
+export default OrderPlaced
