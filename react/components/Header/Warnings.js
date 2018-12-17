17b39e6d2aa552bb74063434eb14c33e830fb5a6
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-17 10:56:46 -0200

    Update Warning section layout

--- react/components/Header/Warnings.js
+++ react/components/Header/Warnings.js
@@ -4,27 +4,27 @@ import React from 'react'
 import { IconWarning } from 'vtex.styleguide'
 
 const Info = () => {
-  const twoOrders = false
+  const twoOrders = true
   return (
     <div>
-      <p><strong>Informações importantes:</strong></p>
-      <ul className="no-list-style mv9">
+      <p><IconWarning color="#FFB100" /><strong className="pl3">Informações importantes:</strong></p>
+      <ul className="mv9 list ml0 pl0">
         <li className="mv7">
-          <IconWarning color="#FFB100" /> <strong> Aprovação do pagamento </strong><br />
+          <strong> Aprovação do pagamento </strong><br />
           Pode demorar até 3 dias.
         </li>
         <li className="mv7">
-          <IconWarning color="#FFB100" /> <strong> Prazo de entrega </strong><br />
+          <strong> Prazo de entrega </strong><br />
           Se inicia a partir do momento em que o pagamento é confirmado.
         </li>
         <li className="mv7">
-          <IconWarning color="#FFB100" /> <strong> Rastreamento </strong><br />
+          <strong> Rastreamento </strong><br />
           Quando seu pedido estiver a caminho, o código de rastreamento será enviado no seu e-mail.
         </li>
 
         {twoOrders
           ? <li className="mv7">
-            <IconWarning color="#FFB100" /> <strong> Sua compra foi dividida em 2 pedidos </strong><br />
+            <strong> Sua compra foi dividida em 2 pedidos </strong><br />
               Alguns itens da sua compra foram vendidos por lojas parceiras, logo eles tiveram que ser separados em outro pedido.
               Suas configurações da compra não foram impactadas.
           </li>
