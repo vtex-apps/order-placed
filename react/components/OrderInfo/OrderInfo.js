4e9b0e02d96e36724945198f57435d915c4cbcbd
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-17 14:15:17 -0200

    Render new components `StorePickUpInfo` and `Summary`

--- react/components/OrderInfo/OrderInfo.js
+++ react/components/OrderInfo/OrderInfo.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import BasicInfo from './BasicInfo'
 import ShippingInfo from './ShippingInfo'
+import StorePickUpInfo from './StorePickUpInfo'
+import Summary from './Summary'
 
 const OrderInfo = () => {
   return (
-    <div>
+    <div className="mv5">
       <BasicInfo />
       <ShippingInfo />
+      <StorePickUpInfo />
+      <Summary />
     </div>
   )
 }
