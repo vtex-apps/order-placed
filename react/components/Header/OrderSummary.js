81dfb6ea96c10868ce0715f67e69337b602113bc
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-17 10:56:11 -0200

    Add margins to boxes

--- react/components/Header/OrderSummary.js
+++ react/components/Header/OrderSummary.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import { Box } from 'vtex.styleguide'
 
 const OrderSummary = () => {
@@ -7,12 +6,16 @@ const OrderSummary = () => {
     <div>
       <p className="f5"><strong>Resumo da sua compra:</strong></p>
       <div className="flex justify-center mv8">
-        <Box>
-          <h3>A receber</h3>
-        </Box>
-        <Box>
-          <h3>A retirar</h3>
-        </Box>
+        <div className="mr4">
+          <Box>
+            <h3>A receber</h3>
+          </Box>
+        </div>
+        <div className="ml4">
+          <Box>
+            <h3>A retirar</h3>
+          </Box>
+        </div>
       </div>
     </div>
   )
