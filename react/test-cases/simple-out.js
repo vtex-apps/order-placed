36fe5a93e667a6842c30d4c47e597a4a4de2dcab
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-18 19:51:58 -0200

    Add response from a OrderGroup query on a real OrderGroup

---
+++ react/test-cases/simple-out.js
@@ -0,0 +1,143 @@
+export const orderGroupQuery = {
+  'data': {
+    'orderGroup': [
+      {
+        'orderId': '884450077058-01',
+        'orderGroup': '884450077058',
+        'state': 'payment-approved',
+        'creationDate': '2018-12-18T21:01:24.0206862Z',
+        'items': [
+          {
+            'uniqueId': 'A3942733B81D43EC8BF608145336191E',
+            'id': '42',
+            'productId': '29',
+            'name': 'PRODUTO 16',
+            'skuName': 'PRODUTO 16',
+            'tax': 0,
+            'price': 1000,
+            'listPrice': 1000,
+            'sellingPrice': 1000,
+            'quantity': 5,
+            'seller': '1',
+            'imageUrl': 'http://recorrenciaqa.vteximg.com.br/arquivos/ids/155422-55-55/mambo.png?v=636093678344600000',
+            'detailUrl': '/aceto-balsamico-costazzurra-vidro-500ml/p',
+          },
+          {
+            'uniqueId': '43F4F7BACC464D2FA9D65613F8B77032',
+            'id': '27',
+            'productId': '14',
+            'name': 'PRODUTO RNB - Preço apenas no SC 2 Preço SC 2',
+            'skuName': 'Preço SC 2',
+            'tax': 0,
+            'price': 2000,
+            'listPrice': 2000,
+            'sellingPrice': 2000,
+            'quantity': 2,
+            'seller': '1',
+            'imageUrl': 'http://recorrenciaqa.vteximg.com.br/arquivos/ids/155407-55-55/mambo.png?v=636093678255400000',
+            'detailUrl': '/abacaxi-em-calda-helomar-400g/p',
+          },
+        ],
+        'shippingData': {
+          'logisticsInfo': [
+            {
+              'itemIndex': 0,
+              'shippingEstimate': null,
+              'shippingEstimateDate': null,
+              'selectedSla': 'PAC',
+              'slas': [
+                {
+                  'id': 'PAC',
+                  'name': 'PAC',
+                  'price': 0,
+                  'shippingEstimate': '4bd',
+                  'deliveryWindow': null,
+                  'deliveryChannel': 'delivery',
+                },
+                {
+                  'id': 'Normal',
+                  'name': 'Normal',
+                  'price': 357,
+                  'shippingEstimate': '3bd',
+                  'deliveryWindow': null,
+                  'deliveryChannel': 'delivery',
+                },
+              ],
+              'deliveryIds': null,
+            },
+            {
+              'itemIndex': 1,
+              'shippingEstimate': null,
+              'shippingEstimateDate': null,
+              'selectedSla': 'PAC',
+              'slas': [
+                {
+                  'id': 'PAC',
+                  'name': 'PAC',
+                  'price': 0,
+                  'shippingEstimate': '4bd',
+                  'deliveryWindow': null,
+                  'deliveryChannel': 'delivery',
+                  'pickupStoreInfo': {
+                    'additionalInfo': null,
+                    'address': null,
+                    'dockId': null,
+                    'friendlyName': null,
+                    'isPickupStore': false,
+                  },
+                },
+                {
+                  'id': 'Normal',
+                  'name': 'Normal',
+                  'price': 143,
+                  'shippingEstimate': '3bd',
+                  'deliveryWindow': null,
+                  'deliveryChannel': 'delivery',
+                  'pickupStoreInfo': {
+                    'additionalInfo': null,
+                    'address': null,
+                    'dockId': null,
+                    'friendlyName': null,
+                    'isPickupStore': false,
+                  },
+                },
+              ],
+              'deliveryIds': null,
+            },
+          ],
+          'address': {
+            'addressId': 'bcff3e538b0b488483b73f0e036ebb4e',
+            'addressType': 'residential',
+            'receiverName': 'Victor Hugo',
+            'city': 'Rio de Janeiro',
+            'state': 'RJ',
+            'street': 'Praia de Botafogo',
+            'number': '53',
+            'neighborhood': 'Botafogo',
+            'complement': null,
+          },
+        },
+        'packageAttachment': null,
+        'paymentData': {
+          'transactions': [
+            {
+              'transactionId': 'C0EA303A45AB4417BF24073DAF05BFF1',
+              'payments': [
+                {
+                  'paymentSystemName': 'Visa',
+                  'paymentSystem': '2',
+                  'lastDigits': '2324',
+                  'group': 'creditCard',
+                },
+              ],
+            },
+          ],
+        },
+        'value': 9000,
+        'storePreferencesData': {
+          'currencyCode': 'BRL',
+        },
+      },
+    ],
+  },
+}
