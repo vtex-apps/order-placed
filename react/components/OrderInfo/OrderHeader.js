413bd325430796748b934a7b84f3026c24368223
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-21 11:37:37 -0200

    Fix CSS for mobile screens

--- react/components/OrderInfo/OrderHeader.js
+++ react/components/OrderInfo/OrderHeader.js
@@ -5,7 +5,7 @@ import { FormattedDate } from 'vtex.order-details'
 
 const OrderHeader = ({ orderInfo }) => {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center flex-wrap">
       <div>
         <p className="t-heading-3-ns t-heading-4">
           Pedido #{orderInfo.orderId}
@@ -15,13 +15,13 @@ const OrderHeader = ({ orderInfo }) => {
           </small>
         </p>
       </div>
-      <div className="flex justify-between">
-        <div className="mr3">
+      <div className="flex justify-between flex-wrap">
+        <div className="mr3-ns mb4-s mb0-m">
           <Button variation="secondary">
           Alterar pedido
           </Button>
         </div>
-        <div className="mr3">
+        <div className="mr3-ns mb4-s mb0-m">
           <Button variation="secondary">
             Ir para seus pedidos
           </Button>
