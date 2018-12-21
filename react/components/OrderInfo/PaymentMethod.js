e44687bc6f4b5e3b974d89b93816a0fdef753ada
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-21 11:55:36 -0200

    Add PaymentFlagPicker to component

--- react/components/OrderInfo/PaymentMethod.js
+++ react/components/OrderInfo/PaymentMethod.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { FormattedPrice } from 'vtex.order-details'
+import { PaymentFlagPicker } from 'vtex.payment-flags'
 
 const paymentGroupSwitch = (payment) => {
   switch (payment) {
@@ -16,6 +17,15 @@ const PaymentMethod = ({ pay }) => {
   return (
     <div className="flex flex-column justify-around mr7-ns">
       <p className="t-heading-6">{paymentGroupSwitch(pay.group)}</p>
+      <PaymentFlagPicker paymentSystem={pay.paymentSystem}>
+        {FlagComponent =>
+          FlagComponent && (
+            <div className="h2">
+              <FlagComponent />
+            </div>
+          )
+        }
+      </PaymentFlagPicker>
       <p>Final {pay.lastDigits}</p>
       <div>
         <FormattedPrice value={pay.value} currency="BRL" />{` (${pay.installments}x)`}
