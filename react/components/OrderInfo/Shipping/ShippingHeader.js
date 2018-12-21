a67153b74f6bfc354ffe1648e0f09569dac37b4a
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-21 15:06:02 -0200

    Create `ShippingHeader` component and refactor `ShippingInfo`

---
+++ react/components/OrderInfo/Shipping/ShippingHeader.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { PrettyAddress } from 'vtex.order-details'
+
+const ShippingHeader = ({ shippingData }) => {
+  return (
+    <div className="flex justify-around flex-wrap">
+      <div>
+        <p className="t-heading-4-ns t-heading-5 self-start">
+          Entrega em casa
+          <br />
+          <small className="c-muted-2 t-small">
+            Em até {shippingData.shippingEstimate} dias úteis
+          </small>
+        </p>
+      </div>
+      <PrettyAddress address={shippingData.address} />
+    </div>
+  )
+}
+
+ShippingHeader.propTypes = {
+  shippingData: PropTypes.object.isRequired,
+}
+
+export default ShippingHeader
