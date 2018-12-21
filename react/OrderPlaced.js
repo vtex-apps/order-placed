2979798dc64a905fd96ef5b0a281dcdb287be330
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-21 10:50:28 -0200

    Add support for multiple orders and pass props to children
    Import data objects from `simple-out` to use as mock data
    and pass relevant information as props to child components.
    
    Also, `OrderPlaced` can support multiple orders.

--- react/OrderPlaced.js
+++ react/OrderPlaced.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 // import { compose, graphql } from 'react-apollo'
 
 // import getOrderGroup from './graphql/getOrderGroup.graphql'
 import Header from './components/Header/Header'
 import OrderInfo from './components/OrderInfo/OrderInfo'
+import { orderGroupQuery, profileQuery } from './test-cases/simple-out'
 
 class OrderPlaced extends Component {
   render() {
     return (
-      <div className="w-50-ns w-90 center">
-        <Header />
-        <hr className="bg-muted-5 c-muted-5 w-95" size="1" />
-        <OrderInfo />
+      <div className="w-80-ns w-90 center">
+        <Header data={orderGroupQuery.orderGroup} profile={profileQuery.profile} />
+        {
+          orderGroupQuery.orderGroup.map((order, index) => {
+            return (
+              <OrderInfo
+                data={order}
+                key={index}
+                idx={index}
+                profile={profileQuery.profile}
+              />
+            )
+          })
+        }
       </div>
     )
   }
 }
 
-// OrderPlaced.propTypes = {
-//   data: PropTypes.object.isRequired,
-// }
+OrderPlaced.propTypes = {
+  data: PropTypes.object.isRequired,
+}
 
 // export default compose(
 //   OrderPlaced,
