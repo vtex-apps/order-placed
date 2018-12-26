5ffacef29806f9893565e9b2a5eae29cf73540cb
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-26 13:32:35 -0200

    Clean up conditionals in `return` statement

--- react/components/Header/Warnings.js
+++ react/components/Header/Warnings.js
@@ -1,34 +1,38 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-const Info = ({ split }) => (
-  <Fragment>
-    <ul className="mv7 list ml0 pl0 t-body bt bb b--muted-4">
-      <li className="tc pv2 mv0">
-        <p className="pv4">A aprovação do pagamento pode demorar até 3 dias.</p>
-        <hr className="mw4 c-muted-5" size="1" />
-      </li>
-      <li className="tc pv2 mv0">
-        <p className="pv4">O prazo de entrega se inicia a partir do momento em que o pagamento é confirmado.</p>
-        <hr className="mw4 c-muted-5" size="1" />
-      </li>
-      <li className="tc pv2 mv0">
-        <p className="pv4">Quando seu pedido estiver a caminho, o código de rastreamento será enviado para o seu e-mail.</p>
-        {
-          (split > 1) && <hr className="mw4 c-muted-5" size="1" />
+const Info = ({ split }) => {
+  const orderWasSplit = (split > 1)
+
+  return (
+    <Fragment>
+      <ul className="mv7 list ml0 pl0 t-body bt bb b--muted-4">
+        <li className="tc pv2 mv0">
+          <p className="pv4">A aprovação do pagamento pode demorar até 3 dias.</p>
+          <hr className="mw4 c-muted-5" size="1" />
+        </li>
+        <li className="tc pv2 mv0">
+          <p className="pv4">O prazo de entrega se inicia a partir do momento em que o pagamento é confirmado.</p>
+          <hr className="mw4 c-muted-5" size="1" />
+        </li>
+        <li className="tc pv2 mv0">
+          <p className="pv4">Quando seu pedido estiver a caminho, o código de rastreamento será enviado para o seu e-mail.</p>
+          {
+            orderWasSplit && <hr className="mw4 c-muted-5" size="1" />
+          }
+        </li>
+
+        {orderWasSplit &&
+        <li className="tc pv6 mv0">
+          Alguns itens da sua compra foram vendidos por lojas parceiras, logo eles tiveram que ser separados em outro pedido.
+          Suas configurações da compra não foram impactadas.
+        </li>
         }
-      </li>
 
-      {(split > 1) &&
-      <li className="tc pv6 mv0">
-        Alguns itens da sua compra foram vendidos por lojas parceiras, logo eles tiveram que ser separados em outro pedido.
-        Suas configurações da compra não foram impactadas.
-      </li>
-      }
-
-    </ul>
-  </Fragment>
-)
+      </ul>
+    </Fragment>
+  )
+}
 
 Info.propTypes = {
   split: PropTypes.number.isRequired,
