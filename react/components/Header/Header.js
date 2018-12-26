1fd6d934fef0a640571d42534be5e5e11eec935c
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-26 13:26:14 -0200

    Update CSS classes and remove unnecessary comments

--- react/components/Header/Header.js
+++ react/components/Header/Header.js
@@ -6,7 +6,7 @@ import { Button, IconSuccess } from 'vtex.styleguide'
 const Header = ({ data, profile }) => (
   <div className="pt7 sans-serif">
 
-    <div className="flex justify-center green">
+    <div className="flex justify-center c-success">
       <IconSuccess size="50" />
     </div>
 
@@ -14,7 +14,6 @@ const Header = ({ data, profile }) => (
         Obrigado por sua compra!
     </p>
 
-    {/* E-mail reminder */}
     <div className="center mt4 t-body tc c-muted-1">
       <p>
         Você receberá um e-mail no endereço <strong className="nowrap">{profile.email}</strong> com todos os detalhes do seu pedido em até 5 minutos.<br />
@@ -22,7 +21,6 @@ const Header = ({ data, profile }) => (
       </p>
     </div>
 
-    {/* Actions */}
     <div className="flex justify-center t-action">
       <p className="tr c-action-primary mr2">
         <Button variation="secondary">
@@ -36,7 +34,6 @@ const Header = ({ data, profile }) => (
       </p>
     </div>
 
-    {/* Important warnings */}
     <Warnings split={data.length} />
   </div>
 )
