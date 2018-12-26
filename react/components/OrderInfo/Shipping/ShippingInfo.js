4916dd2460124996382cc481f8203189bf87313e
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-26 12:03:02 -0200

    Remove unnecessary comments

--- react/components/OrderInfo/Shipping/ShippingInfo.js
+++ react/components/OrderInfo/Shipping/ShippingInfo.js
@@ -6,29 +6,7 @@ import ShippingHeader from './ShippingHeader'
 const ShippingInfo = ({ data }) => {
   return (
     <div className="mv8 flex flex-column justify-between">
-      {/* <h2>
-        <span className="ml4">Entrega em casa</span>
-        <small className="t-small">Em até {data.shippingEstimate} dias úteis</small>
-      </h2> */}
       <ShippingHeader shippingData={data.shippingData} />
-      <div className="mb6">
-        {/* <Box>
-          <div className="flex flex-column justify-between">
-            <div>
-              <p>{data.Address.street}, {data.Address.number}</p>
-              <p>CEP 04317000 - {data.Address.neighborhood}</p>
-              <p>{data.Address.city}, {data.shippingAddress.state} </p>
-            </div>
-            <div>
-              {
-                data.items.filter((item) => (item.delivery === 'shipping')).map(item => {
-                  return (<p key="item.name">{item.name}</p>)
-                })
-              }
-            </div>
-          </div>
-        </Box> */}
-      </div>
     </div>
   )
 }
