6809e0b20e175c56c15313870a92a009248bf8e8
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-27 17:44:51 -0200

    Remove extra <div> tags

--- react/components/OrderInfo/Shipping/ShippingHeader.js
+++ react/components/OrderInfo/Shipping/ShippingHeader.js
@@ -4,15 +4,13 @@ import { PrettyAddress } from 'vtex.order-details'
 
 const ShippingHeader = ({ shippingData }) => (
   <div className="flex flex-column">
-    <div>
-      <p className="t-heading-4-ns t-heading-5">
+    <p className="t-heading-4-ns t-heading-5">
         Entrega em casa
-        <br />
-        <small className="c-muted-2 t-small">
+      <br />
+      <small className="c-muted-2 t-small">
           Em até {shippingData.shippingEstimate} dias úteis
-        </small>
-      </p>
-    </div>
+      </small>
+    </p>
     <PrettyAddress address={shippingData.address} />
   </div>
 )
--- react/components/OrderInfo/StorePickUp/StorePickUpHeader.js
+++ react/components/OrderInfo/StorePickUp/StorePickUpHeader.js
@@ -3,15 +3,13 @@ import PropTypes from 'prop-types'
 
 const StorePickUpHeader = ({ shippingData }) => (
   <div className="flex flex-column flex-wrap">
-    <div>
-      <p className="t-heading-4-ns t-heading-5 self-start">
+    <p className="t-heading-4-ns t-heading-5 self-start">
         Retirada no ponto
-        <br />
-        <small className="c-muted-2 t-small">
+      <br />
+      <small className="c-muted-2 t-small">
           Pronto em até {shippingData.shippingEstimate} dias úteis
-        </small>
-      </p>
-    </div>
+      </small>
+    </p>
   </div>
 )
 
