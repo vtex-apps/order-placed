9e257824bb83f5fc80377344639452b499e72700
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-27 17:03:17 -0200

    Remove unnecessary CSS classes

--- react/components/OrderInfo/Shipping/ShippingHeader.js
+++ react/components/OrderInfo/Shipping/ShippingHeader.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 import { PrettyAddress } from 'vtex.order-details'
 
 const ShippingHeader = ({ shippingData }) => (
-  <div className="flex flex-column flex-wrap">
+  <div className="flex flex-column">
     <div>
-      <p className="t-heading-4-ns t-heading-5 self-start">
+      <p className="t-heading-4-ns t-heading-5">
         Entrega em casa
         <br />
         <small className="c-muted-2 t-small">
