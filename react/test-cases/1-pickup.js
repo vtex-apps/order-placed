c82ffd3c2588209c0f5c0987009c28810e9bca96
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-28 15:54:43 -0200

    Extract profileQuery and add new test cases

--- react/test-cases/1-pickup.js
+++ react/test-cases/1-pickup.js
@@ -1,14 +1,3 @@
-export const profileQuery = {
-  'profile': {
-    'id': '37faf0df-4164-4fab-b80f-5e1b9957b411',
-    'firstName': 'Victor',
-    'lastName': 'Hugo',
-    'email': 'victor2142@gmail.com',
-    'document': '45325123440',
-    'phone': '+5511983768051',
-  },
-}
-
 export const orderGroupQuery = {
   'orderGroup': [
     {
