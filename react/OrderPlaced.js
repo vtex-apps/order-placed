f4fafc61e00be34b67406bc275bfca7b858b847d
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-12-28 11:03:33 -0200

    Add more complex orderGroup output

--- react/OrderPlaced.js
+++ react/OrderPlaced.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Header from './components/Header/Header'
 import OrderInfo from './components/OrderInfo/OrderInfo'
-import { orderGroupQuery, profileQuery } from './test-cases/simple-out'
+import { orderGroupQuery, profileQuery } from './test-cases/two-payment'
 
 class OrderPlaced extends Component {
   render() {
