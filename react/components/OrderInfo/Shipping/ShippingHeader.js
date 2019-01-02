6eddcdf1a259f61778f2bb1f1240a9ee5cea8e29
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-02 12:04:51 -0200

    Use `parcelify` from `@vtex/delivery-packages` to split order
    Split orders according to their `deliveryChannel` and `shippingEstimate`.
    
    Pass down each group to render `Shipping` and `StorePickUp` components.

--- react/components/OrderInfo/Shipping/ShippingHeader.js
+++ react/components/OrderInfo/Shipping/ShippingHeader.js
@@ -5,10 +5,10 @@ import { PrettyAddress } from 'vtex.order-details'
 const ShippingHeader = ({ shippingData }) => (
   <div className="flex flex-column">
     <p className="t-heading-4-ns t-heading-5">
-        Entrega em casa
+      Entrega em casa
       <br />
       <small className="c-muted-2 t-small">
-          Em até {shippingData.shippingEstimate} dias úteis
+        Em até {shippingData.shippingEstimate} dias úteis
       </small>
     </p>
     <PrettyAddress address={shippingData.address} />
