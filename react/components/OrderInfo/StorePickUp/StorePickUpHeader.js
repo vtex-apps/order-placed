aa16c7114efb1e5d2021de7e7c36754f00640fed
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-02 15:02:06 -0200

    Update StorePickUp rendering

--- react/components/OrderInfo/StorePickUp/StorePickUpHeader.js
+++ react/components/OrderInfo/StorePickUp/StorePickUpHeader.js
@@ -4,17 +4,17 @@ import PropTypes from 'prop-types'
 const StorePickUpHeader = ({ shippingData }) => (
   <div className="flex flex-column flex-wrap">
     <p className="t-heading-4-ns t-heading-5 self-start">
-        Retirada no ponto
+      Retirada no ponto
       <br />
       <small className="c-muted-2 t-small">
-          Pronto em até {shippingData.shippingEstimate} dias úteis
+        Pronto em até {shippingData.shippingEstimate} dias úteis
       </small>
     </p>
   </div>
 )
 
 StorePickUpHeader.propTypes = {
-  shippingData: PropTypes.object.isRequired,
+  shippingData: PropTypes.array.isRequired,
 }
 
 export default StorePickUpHeader
