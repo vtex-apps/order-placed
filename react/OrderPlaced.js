240d0826a8e6f218dacac0a75d725337d5856062
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-02 15:01:49 -0200

    Pass new `currency` prop and update test case being used for mixed orders

--- react/OrderPlaced.js
+++ react/OrderPlaced.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Header from './components/Header/Header'
 import OrderInfo from './components/OrderInfo/OrderInfo'
-import { orderGroupQuery } from './test-cases/1-delivery'
+import { orderGroupQuery } from './test-cases/3-pickup-only-2-delivery-only'
 import { profileQuery } from './test-cases/profileQuery'
 
 class OrderPlaced extends Component {
@@ -17,6 +17,7 @@ class OrderPlaced extends Component {
                 data={order}
                 key={order.orderId}
                 profile={profileQuery.profile}
+                currency={order.storePreferencesData.currencyCode}
               />
             )
           })
