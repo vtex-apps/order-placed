b89f660cf48c697ba1fde772d737e6cd169a2027
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-02 16:29:14 -0200

    Create new variables to not change `props`

--- react/components/OrderInfo/Shipping/Shipping.js
+++ react/components/OrderInfo/Shipping/Shipping.js
@@ -4,11 +4,11 @@ import ShippingHeader from './ShippingHeader'
 import ProductList from '../ProductList'
 
 const Shipping = ({ data, currency }) => {
-  data = data.filter(parcel => (parcel.deliveryChannel === 'delivery'))
+  const deliveryPackages = data.filter(parcel => (parcel.deliveryChannel === 'delivery'))
   return (
     <Fragment>
       {
-        data.map((delivery, index) => (
+        deliveryPackages.map((delivery, index) => (
           <div className="mv8 flex flex-column justify-between" key={index}>
             <ShippingHeader shippingData={delivery} />
             <ProductList products={delivery.items} currency={currency} />
--- react/components/OrderInfo/StorePickUp/StorePickUp.js
+++ react/components/OrderInfo/StorePickUp/StorePickUp.js
@@ -5,13 +5,13 @@ import StorePickUpHeader from './StorePickUpHeader'
 import ProductList from '../ProductList'
 
 const StorePickUp = ({ data, currency }) => {
-  data = data.filter(parcel => (parcel.deliveryChannel === 'pickup-in-point'))
+  const pickUpPackages = data.filter(parcel => (parcel.deliveryChannel === 'pickup-in-point'))
   return (
     <Fragment>
       {
-        data.map((pickup, index) => (
+        pickUpPackages.map((pickup, index) => (
           <div className="mv8 flex flex-column justify-between" key={index}>
-            <StorePickUpHeader shippingData={data} />
+            <StorePickUpHeader shippingData={pickup} />
             <ProductList products={pickup.items} currency={currency} />
           </div>
         ))
