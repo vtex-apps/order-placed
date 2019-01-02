03790b8088824fc5c379fc12af93ad98ad224217
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-02 12:04:51 -0200

    Use `parcelify` from `@vtex/delivery-packages` to split order
    Split orders according to their `deliveryChannel` and `shippingEstimate`.
    
    Pass down each group to render `Shipping` and `StorePickUp` components.

--- react/components/OrderInfo/OrderInfo.js
+++ react/components/OrderInfo/OrderInfo.js
@@ -7,33 +7,39 @@ import Shipping from './Shipping/Shipping'
 import OrderTotal from './OrderTotal'
 import StorePickUp from './StorePickUp/StorePickUp'
 
-const OrderInfo = ({ data, profile }) => (
-  <div className="mv6 w-80-ns w-90 center">
-    <OrderHeader orderInfo={data} />
-    <div className="bb b--muted-5">
-      <CustumerInfo profile={profile} />
+const parcelify = require('@vtex/delivery-packages')
+
+const OrderInfo = ({ data, profile, currency }) => {
+  const parcels = parcelify(data)
+  return (
+    <div className="mv6 w-80-ns w-90 center">
+      <OrderHeader orderInfo={data} />
+      <div className="bb b--muted-5">
+        <CustumerInfo profile={profile} />
+      </div>
+      <div className="bb b--muted-5">
+        <PaymentSummary paymentsData={data.paymentData.transactions[0].payments} />
+      </div>
+      <div className="bb b--muted-5">
+        <Shipping data={parcels} currency={currency} />
+      </div>
+      <div className="bb b--muted-5">
+        <StorePickUp data={parcels} currency={currency} />
+      </div>
+      <OrderTotal
+        items={data.items}
+        currency={data.storePreferencesData.currencyCode}
+        totals={data.totals}
+        orderValue={data.value}
+      />
     </div>
-    <div className="bb b--muted-5">
-      <PaymentSummary paymentsData={data.paymentData.transactions[0].payments} />
-    </div>
-    <div className="bb b--muted-5">
-      <Shipping data={data} />
-    </div>
-    <div className="bb b--muted-5">
-      <StorePickUp data={data} />
-    </div>
-    <OrderTotal
-      items={data.items}
-      currency={data.storePreferencesData.currencyCode}
-      totals={data.totals}
-      orderValue={data.value}
-    />
-  </div>
-)
+  )
+}
 
 OrderInfo.propTypes = {
   data: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
+  currency: PropTypes.string.isRequired,
 }
 
 export default OrderInfo
--- react/components/OrderInfo/Shipping/Shipping.js
+++ react/components/OrderInfo/Shipping/Shipping.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import ShippingHeader from './ShippingHeader'
 import ShippingProducts from './ShippingProducts'
 
-const Shipping = ({ data }) => (
-  <div className="mv8 flex flex-column justify-between">
-    <ShippingHeader shippingData={data.shippingData} />
-    <ShippingProducts products={data.items} currency={data.storePreferencesData.currencyCode} />
-  </div>
-)
+const Shipping = ({ data, currency }) => {
+  data = data.filter(parcel => (parcel.deliveryChannel === 'delivery'))
+  return (
+    <Fragment>
+      {
+        data.map((delivery, index) => (
+          <div className="mv8 flex flex-column justify-between" key={index}>
+            <ShippingHeader shippingData={delivery} />
+            <ShippingProducts products={delivery.items} currency={currency} />
+          </div>
+        ))
+      }
+    </Fragment>
+  )
+}
 
 Shipping.propTypes = {
-  data: PropTypes.object.isRequired,
+  data: PropTypes.array.isRequired,
+  currency: PropTypes.string.isRequired,
 }
 
 export default Shipping
