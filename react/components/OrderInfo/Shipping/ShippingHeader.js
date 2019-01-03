e1d9e82edef13380aeb784a17be76b38f109e820
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-03 14:54:37 -0200

    Update `ShippingHeader` to count each delivery

--- react/components/OrderInfo/Shipping/ShippingHeader.js
+++ react/components/OrderInfo/Shipping/ShippingHeader.js
@@ -3,18 +3,24 @@ import PropTypes from 'prop-types'
 import { PrettyAddress } from 'vtex.order-details'
 import TranslateEstimate from 'vtex.shipping-estimate-translator/TranslateEstimate'
 
-const ShippingHeader = ({ shippingData }) => (
-  <div className="flex flex-column">
-    <p className="t-heading-4-ns t-heading-5">
-      Entrega em casa
-      <br />
-      <small className="c-muted-2 t-small">
-        <TranslateEstimate shippingEstimate={shippingData.shippingEstimate} />
-      </small>
-    </p>
-    <PrettyAddress address={shippingData.address} />
-  </div>
-)
+const ShippingHeader = ({ shippingData, index, numPackages }) => {
+  const multipleDeliveries = (numPackages > 1)
+  return (
+    <div className="flex flex-column">
+      <p className="t-heading-4-ns t-heading-5">
+        Entrega em casa
+        {
+          multipleDeliveries
+          && ` - n˚ ${index+1} de ${numPackages}`
+        }
+        <br />
+        <small className="c-muted-2 t-small">
+          <TranslateEstimate shippingEstimate={shippingData.shippingEstimate} />
+        </small>
+      </p>
+      <PrettyAddress address={shippingData.address} />
+    </div>
+)}
 
 ShippingHeader.propTypes = {
   shippingData: PropTypes.object.isRequired,
