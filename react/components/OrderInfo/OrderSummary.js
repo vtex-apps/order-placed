04b10ee67f54abf077054b00a57abdeb41691744
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-03 19:05:09 -0200

    Add shipping estimates to `OrderSummary`

--- react/components/OrderInfo/OrderSummary.js
+++ react/components/OrderInfo/OrderSummary.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Box } from 'vtex.styleguide'
 import { parcelShape } from '../../proptypes/shapes'
+import estimateCalculator from '@vtex/estimate-calculator'
+import TranslateEstimate from 'vtex.shipping-estimate-translator/TranslateEstimate'
 
 const OrderSummary = ({ delivery, pickup }) => {
   let deliveryItemsQuantity = 0
@@ -10,6 +12,9 @@ const OrderSummary = ({ delivery, pickup }) => {
   let pickUpItemsQuantity = 0
   pickup.forEach(pickupPackage => { pickUpItemsQuantity += pickupPackage.items.length })
 
+  const longestDeliveryEstimate = estimateCalculator.getLatestSla(delivery)
+  const longestPickUpEstimate = estimateCalculator.getLatestSla(pickup)
+
   return (
     <div className="flex justify-around mv7 fleex-wrap">
       <div className="w-40-l w-90">
@@ -17,7 +22,7 @@ const OrderSummary = ({ delivery, pickup }) => {
           <p className="t-heading-4 tc">A receber</p>
           <hr className="bg-muted-4 bt b--muted-4" />
           <p><strong>{`${deliveryItemsQuantity} itens`}</strong>{`, separados em ${delivery.length} entregas`}</p>
-          <p className="t-heading-4">ShippingEstimate</p>
+          <p className="t-heading-4"><TranslateEstimate shippingEstimate={longestDeliveryEstimate.shippingEstimate}/></p>
           <small className="t-small">{`Em ${delivery[0].address.street}, ${delivery[0].address.number}`}</small>
         </Box>
       </div>
@@ -26,8 +31,8 @@ const OrderSummary = ({ delivery, pickup }) => {
           <p className="t-heading-4 tc">A retirar</p>
           <hr className="bg-muted-4 bt b--muted-4" />
           <p><strong>{`${pickUpItemsQuantity} itens`}</strong>{`, separados em ${pickup.length} retiradas`}</p>
-          <p className="t-heading-4">ShippingEstimate</p>
-          <small className="t-small">{`Em ${pickup[0].selectedSlaObj.friendlyName}`}</small>
+          <p className="t-heading-4"><TranslateEstimate shippingEstimate={longestPickUpEstimate.shippingEstimate}/></p>
+          <small className="t-small">{`Em ${pickup[0].pickupFriendlyName}`}</small>
         </Box>
       </div>
     </div>
