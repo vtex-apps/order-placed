693b7ed185737eac47e7e9f3f79337690d7ed973
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-04 15:36:23 -0200

    Add conditional rendering for `StorePickUp` and `Shipping` components

--- react/components/OrderInfo/OrderInfo.js
+++ react/components/OrderInfo/OrderInfo.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import parcelify from '@vtex/delivery-packages'
 import OrderHeader from './OrderHeader'
@@ -6,7 +6,6 @@ import CustumerInfo from './CustumerInfo'
 import PaymentSummary from '../Payment/PaymentSummary'
 import Shipping from '../Shipping/Shipping'
 import OrderTotal from './OrderTotal'
-import OrderSummary from './OrderSummary'
 import StorePickUp from '../StorePickUp/StorePickUp'
 import OrderSplitNotice from './OrderSplitNotice'
 import OrderSection from './OrderSection'
@@ -20,37 +19,41 @@ const OrderInfo = ({ data, profile }) => {
   const takeaway = parcels.filter((takeawayPackage) => takeawayPackage.deliveryChannel === 'takeaway')
   const multipleDeliveries = (delivery.length > 1)
   return (
-    <div className="mv6 w-80-ns w-90 center">
-      <OrderSection>
-        <OrderSummary delivery={delivery} pickup={pickup} />
-      </OrderSection>
-      <OrderHeader orderInfo={data} />
-      {
-        multipleDeliveries
-        && <OrderSplitNotice
-            deliveries={delivery.length}
-            pickups={pickup.length}
-            takeaways={takeaway.length}
-          />
-      }
-      <OrderSection>
+    <Fragment>
+      <div className="mv6 w-80-ns w-90 center">
+        <OrderHeader orderInfo={data} />
+        {
+          multipleDeliveries
+          && <OrderSplitNotice
+              deliveries={delivery.length}
+              pickups={pickup.length}
+              takeaways={takeaway.length}
+            />
+        }
         <CustumerInfo profile={profile} />
-      </OrderSection>
-      <OrderSection>
-        <PaymentSummary paymentsData={data.paymentData.transactions[0].payments} />
-      </OrderSection>
-      <OrderSection>
-        <StorePickUp pickUpPackages={pickup} />
-      </OrderSection>
-      <OrderSection>
-        <Shipping deliveryPackages={delivery} />
-      </OrderSection>
-      <OrderTotal
-        items={data.items}
-        totals={data.totals}
-        orderValue={data.value}
-      />
-    </div>
+        <OrderSection>
+          <PaymentSummary paymentsData={data.paymentData.transactions[0].payments} />
+        </OrderSection>
+        {
+          (pickup.length > 0) &&
+          (<OrderSection>
+            <StorePickUp pickUpPackages={pickup} />
+          </OrderSection>)
+        }
+        {
+          (delivery.length > 0) &&
+          (<OrderSection>
+            <Shipping deliveryPackages={delivery} />
+          </OrderSection>)
+        }
+        <OrderTotal
+          items={data.items}
+          totals={data.totals}
+          orderValue={data.value}
+        />
+      </div>
+      <hr className="bg-muted-4 bt b--muted-4" />
+    </Fragment>
   )
 }
 
