9e05db77dd14b7b01afffe76cc664004a7f6e460
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-04 16:30:16 -0200

    Add new `Context` to signal if a purshase is split into multiple orders

--- react/OrderPlaced.js
+++ react/OrderPlaced.js
@@ -6,24 +6,27 @@ import { orderGroupQuery } from './test-cases/3-pickup-only-2-delivery-only'
 import { profileQuery } from './test-cases/profileQuery'
 
 export const CurrencyContext = React.createContext('BRL')
+export const SplitOrderContext = React.createContext(false)
 class OrderPlaced extends Component {
   render() {
     return (
       <CurrencyContext.Provider value={orderGroupQuery.orderGroup[0].storePreferencesData.currencyCode}>
-        <div className="center">
-          <Header data={orderGroupQuery.orderGroup} profile={profileQuery.profile} />
-          {
-            orderGroupQuery.orderGroup.map(order => {
-              return (
-                <OrderInfo
-                  data={order}
-                  profile={profileQuery.profile}
-                  key={order.orderId}
-                />
-              )
-            })
-          }
-        </div>
+        <SplitOrderContext.Provider value={orderGroupQuery.orderGroup.length > 1}>
+          <div className="center">
+            <Header data={orderGroupQuery.orderGroup} profile={profileQuery.profile} />
+            {
+              orderGroupQuery.orderGroup.map(order => {
+                return (
+                  <OrderInfo
+                    data={order}
+                    profile={profileQuery.profile}
+                    key={order.orderId}
+                  />
+                )
+              })
+            }
+          </div>
+        </SplitOrderContext.Provider>
       </CurrencyContext.Provider>
     )
   }
