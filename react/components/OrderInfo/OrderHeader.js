480d48b927e5dd4d747075ae31bdedd2ab83ca17
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-07 11:46:02 -0200

    Add `withRuntimeContext` to `OrderHeader` component

--- react/components/OrderInfo/OrderHeader.js
+++ react/components/OrderInfo/OrderHeader.js
@@ -4,49 +4,56 @@ import { Button } from 'vtex.styleguide'
 import { FormattedTime } from 'react-intl'
 import { FormattedDate } from 'vtex.order-details'
 import { SplitOrderContext } from '../../OrderPlaced'
+import { withRuntimeContext } from 'render'
 
-const OrderHeader = ({ orderInfo }) => (
-  <div className="flex justify-between items-center flex-wrap mt7">
-    <p className="t-heading-3-ns t-heading-4 lh-solid">
-      Pedido #{orderInfo.orderId}
-      <br/>
-      <small className="c-muted-2 t-small">
-        Realizado em <FormattedDate date={orderInfo.creationDate} style="short" /> às <FormattedTime value={orderInfo.creationDate} />
-      </small>
-      <br/>
-      <SplitOrderContext.Consumer>
-        {splitOrder =>
-          (splitOrder &&
-            <small className="c-muted-2 t-small">
-              Vendido e entregue por <span className="c-action-primary">{orderInfo.sellers[0].name}</span>
-            </small>
-          )
+const OrderHeader = ({ orderInfo, runtime }) => {
+
+  const storeAccount = runtime.account
+  const orderSeller = orderInfo.sellers[0].name
+
+  return (
+    <div className="flex justify-between items-center flex-wrap mt7">
+      <p className="t-heading-3-ns t-heading-4 lh-solid">
+        Pedido #{orderInfo.orderId}
+        <br/>
+        <small className="c-muted-2 t-small">
+          Realizado em <FormattedDate date={orderInfo.creationDate} style="short" /> às <FormattedTime value={orderInfo.creationDate} />
+        </small>
+        <br/>
+        <SplitOrderContext.Consumer>
+          {splitOrder =>
+            ((splitOrder && storeAccount != orderSeller) &&
+              <small className="c-muted-2 t-small">
+                Vendido e entregue por <span className="c-action-primary">{orderSeller}</span>
+              </small>
+            )
+          }
+        </SplitOrderContext.Consumer>
+      </p>
+      <div className="flex justify-between flex-wrap">
+        <div className="mr3-ns mb4-s mb0-m">
+          <Button variation="secondary">
+            Alterar pedido
+          </Button>
+        </div>
+        <div className="mr3-ns mb4-s mb0-m">
+          <Button variation="secondary">
+            Ir para seus pedidos
+          </Button>
+        </div>
+        {
+          orderInfo.allowCancellation &&
+          (<Button variation="danger-tertiary">
+            Cancelar pedido
+          </Button>)
         }
-      </SplitOrderContext.Consumer>
-    </p>
-    <div className="flex justify-between flex-wrap">
-      <div className="mr3-ns mb4-s mb0-m">
-        <Button variation="secondary">
-          Alterar pedido
-        </Button>
       </div>
-      <div className="mr3-ns mb4-s mb0-m">
-        <Button variation="secondary">
-          Ir para seus pedidos
-        </Button>
-      </div>
-      {
-        orderInfo.allowCancellation &&
-        (<Button variation="danger-tertiary">
-          Cancelar pedido
-        </Button>)
-      }
     </div>
-  </div>
-)
+  )
+}
 
 OrderHeader.propTypes = {
   orderInfo: PropTypes.object.isRequired,
 }
 
-export default OrderHeader
+export default withRuntimeContext(OrderHeader)
