037c42c3b630b62dec4d2e7538eef95f8c5aaff2
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-07 17:18:51 -0200

    Create `FormattedPrice` component and add `CurrencyContext` into it

---
+++ react/components/Payment/FormattedPrice.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { FormattedNumber } from 'react-intl'
+import { CurrencyContext } from '../../OrderPlaced'
+
+const Price = ({ value }) => (
+  <CurrencyContext.Consumer>
+    {
+      currency =>
+        <FormattedNumber currency={currency} style="currency" value={value / 100} />
+    }
+  </CurrencyContext.Consumer>
+)
+
+Price.propTypes = {
+  value: PropTypes.number,
+  currency: PropTypes.string,
+}
+
+export default Price
--- react/components/Payment/PaymentMethod.js
+++ react/components/Payment/PaymentMethod.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { FormattedPrice } from 'vtex.order-details'
 import { PaymentFlagPicker } from 'vtex.payment-flags'
 import { paymentShape } from '../../proptypes/shapes'
-import { CurrencyContext } from '../../OrderPlaced'
+import Price from './FormattedPrice'
 
 const paymentGroupSwitch = (payment) => {
   switch (payment) {
@@ -14,22 +13,20 @@ const paymentGroupSwitch = (payment) => {
 }
 
 const PaymentMethod = ({ payment }) => (
-  <CurrencyContext.Consumer>
-    {currency => (<div className="flex flex-column justify-around mr7-ns">
-      <p className="t-body c-on-base">{paymentGroupSwitch(payment.group)}</p>
-      <PaymentFlagPicker paymentSystem={payment.paymentSystem}>
-        {FlagComponent =>
-          FlagComponent && (
-            <div className="h2">
-              <FlagComponent />
-            </div>
-          )
-        }
-      </PaymentFlagPicker>
-      <p>Final {payment.lastDigits}</p>
-      <FormattedPrice value={payment.value} currency={currency} />{` (${payment.installments}x)`}
-    </div>)}
-  </CurrencyContext.Consumer>
+  <div className="flex flex-column justify-around mr7-ns">
+    <p className="t-body c-on-base">{paymentGroupSwitch(payment.group)}</p>
+    <PaymentFlagPicker paymentSystem={payment.paymentSystem}>
+      {FlagComponent =>
+        FlagComponent && (
+          <div className="h2">
+            <FlagComponent />
+          </div>
+        )
+      }
+    </PaymentFlagPicker>
+    <p>Final {payment.lastDigits}</p>
+    <Price value={payment.value} />{` (${payment.installments}x)`}
+  </div>
 )
 
 PaymentMethod.propTypes = {
