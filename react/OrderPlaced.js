dbcd6b2289c086c870505e030dec59a9f908946d
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-08 17:54:20 -0200

    Add `Loader` component to be displayed while GraphQL query is not complete

--- react/OrderPlaced.js
+++ react/OrderPlaced.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { compose, graphql } from 'react-apollo'
+import { branch, renderComponent } from 'recompose'
+import { Loader } from 'vtex.order-details'
 import Header from './components/Header/Header'
 import OrderInfo from './components/OrderInfo/OrderInfo'
 import { profileQuery } from './test-cases/profileQuery'
 import getOrderGroup from './graphql/getOrderGroup.graphql'
-import withNoSSR from './withNoSSR'
+import withoutSSR from './withoutSSR'
 
 export const CurrencyContext = React.createContext('BRL')
 export const SplitOrderContext = React.createContext(false)
 class OrderPlaced extends Component {
   render() {
     const { orderGroupQuery } = this.props
-    return (orderGroupQuery.loading) ? null
-      : (
-        <CurrencyContext.Provider value={orderGroupQuery.orderGroup[0].storePreferencesData.currencyCode}>
-          <SplitOrderContext.Provider value={orderGroupQuery.orderGroup.length > 1}>
-            <div className="center">
+    return (
+      <CurrencyContext.Provider value={orderGroupQuery.orderGroup[0].storePreferencesData.currencyCode}>
+        <SplitOrderContext.Provider value={orderGroupQuery.orderGroup.length > 1}>
+          <div className="center">
               <Header data={orderGroupQuery.orderGroup} profile={profileQuery.profile} />
               {
-                orderGroupQuery.orderGroup.map(order => {
-                  return (
+                orderGroupQuery.orderGroup.map(order => (
                     <OrderInfo
                       data={order}
                       profile={profileQuery.profile}
                       key={order.orderId}
                     />
                   )
-                })
+                )
               }
             </div>
-          </SplitOrderContext.Provider>
-        </CurrencyContext.Provider>
-      )
+        </SplitOrderContext.Provider>
+      </CurrencyContext.Provider>
+    )
   }
 }
 
@@ -50,6 +50,7 @@ const queryOptions = {
 }
 
 export default compose(
-  withNoSSR,
-  graphql(getOrderGroup, queryOptions)
+  withoutSSR,
+  graphql(getOrderGroup, queryOptions),
+  branch(({ orderGroupQuery }) => orderGroupQuery.loading, renderComponent(Loader))
 )(OrderPlaced)
