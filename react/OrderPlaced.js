fb5ec81f34d895ac8226d2e510c9d742f7f5bfc6
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-08 13:36:44 -0200

    Update component to make the query `getOrderGroup`

--- react/OrderPlaced.js
+++ react/OrderPlaced.js
@@ -1,39 +1,56 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { compose, graphql } from 'react-apollo'
 import Header from './components/Header/Header'
 import OrderInfo from './components/OrderInfo/OrderInfo'
-import { orderGroupQuery } from './test-cases/split-order-2-sellers'
 import { profileQuery } from './test-cases/profileQuery'
+import getOrderGroup from './graphql/getOrderGroup.graphql'
+import withNoSSR from './withNoSSR'
 
 export const CurrencyContext = React.createContext('BRL')
 export const SplitOrderContext = React.createContext(false)
 class OrderPlaced extends Component {
   render() {
-    return (
-      <CurrencyContext.Provider value={orderGroupQuery.orderGroup[0].storePreferencesData.currencyCode}>
-        <SplitOrderContext.Provider value={orderGroupQuery.orderGroup.length > 1}>
-          <div className="center">
-            <Header data={orderGroupQuery.orderGroup} profile={profileQuery.profile} />
-            {
-              orderGroupQuery.orderGroup.map(order => {
-                return (
-                  <OrderInfo
-                    data={order}
-                    profile={profileQuery.profile}
-                    key={order.orderId}
-                  />
-                )
-              })
-            }
-          </div>
-        </SplitOrderContext.Provider>
-      </CurrencyContext.Provider>
-    )
+    const { orderGroupQuery } = this.props
+    console.log(orderGroupQuery)
+    return orderGroupQuery.loading ? null
+      : (
+        <CurrencyContext.Provider value={orderGroupQuery.orderGroup[0].storePreferencesData.currencyCode}>
+          <SplitOrderContext.Provider value={orderGroupQuery.orderGroup.length > 1}>
+            <div className="center">
+              <Header data={orderGroupQuery.orderGroup} profile={profileQuery.profile} />
+              {
+                orderGroupQuery.orderGroup.map(order => {
+                  return (
+                    <OrderInfo
+                      data={order}
+                      profile={profileQuery.profile}
+                      key={order.orderId}
+                    />
+                  )
+                })
+              }
+            </div>
+          </SplitOrderContext.Provider>
+        </CurrencyContext.Provider>
+      )
   }
 }
 
 OrderPlaced.propTypes = {
-  data: PropTypes.object,
+  orderGroupQuery: PropTypes.object.isRequired,
 }
 
-export default OrderPlaced
+const queryOptions = {
+  name: 'orderGroupQuery',
+  options: {
+    variables: {
+      orderGroup: '900863205885',
+    },
+  },
+}
+
+export default compose(
+  withNoSSR,
+  graphql(getOrderGroup, queryOptions)
+)(OrderPlaced)
