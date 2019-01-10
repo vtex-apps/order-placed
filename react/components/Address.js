12e416a2aae060fb75a40f853d909f7083b83f26
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-10 14:40:07 -0200

    Fix import paths for PropType shapes

--- react/components/Address.js
+++ react/components/Address.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { AddressSummary, AddressRules } from 'vtex.address-form'
-
-import { addressShape } from '../proptypes/shapes'
+import { addressShape } from '../shapes'
 
 const Address = ({ address, pickup }) => {
   return (
