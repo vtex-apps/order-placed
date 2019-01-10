5e1a1548e72db20381ccd86e31fe33e75c3af66f
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-10 14:47:27 -0200

    Fix import path

--- react/components/OrderInfo/CustumerInfo.js
+++ react/components/OrderInfo/CustumerInfo.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { profileShape } from '../../proptypes/shapes'
+import { profileShape } from '../../shapes'
 
 const CustumerInfo = ({ profile }) => (
   <div className="flex flex-column">
     <ul className="list pl0 c-on-base">
       <li className="pv2 c-on-base">{`${profile.firstName} ${profile.lastName}`}</li>
       <li className="pv2 c-muted-2">{profile.email}</li>
-      <li className="pv2 c-muted-2">CPF {profile.document}</li>
+      <li className="pv2 c-muted-2">{profile.document}</li>
       <li className="pv2 c-muted-2">{profile.phone}</li>
     </ul>
   </div>
