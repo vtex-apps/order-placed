c7064262f751c9164a68896587d396b00669bdbe
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-17 11:51:55 -0200

    Create `BankInvoice` component

---
+++ react/components/Header/BankInvoice/BankInvoice.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button } from 'vtex.styleguide'
+import { injectIntl, intlShape } from 'react-intl'
+import { intlMessage } from '../../../utils'
+import PrinterIcon from '../../../Icons/PrinterIcon'
+import BarCode from './BarCode'
+import Embeded from './Embeded'
+
+const BankInvoice = ({ url, invoiceBarCodeNumber, intl }) => {
+  return (
+    <div className="pv4 w-80-ns w-90 center bb b--muted-5">
+      <p className="t-heading-4">
+        {intlMessage(intl, 'header.bankinvoice.header')}
+      </p>
+      <div className="flex justify-between items-center">
+        <BarCode barCodeNumber={invoiceBarCodeNumber} />
+        <a href={url}>
+          <Button variation="secondary">
+            <div className="mr3">
+              <PrinterIcon />
+            </div>
+            {intlMessage(intl, 'header.bankinvoice.print')}
+          </Button>
+        </a>
+      </div>
+      <Embeded url={url} />
+    </div>
+  )
+}
+
+BankInvoice.propTypes = {
+  url: PropTypes.string,
+  invoiceBarCodeNumber: PropTypes.string,
+  intl: intlShape.isRequired,
+}
+
+export default injectIntl(BankInvoice)
