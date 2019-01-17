99aae71be11f3113359d90c23271df2a12a18a28
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-17 15:20:30 -0200

    Remove unnecessary `return`s

--- react/components/Header/BankInvoice/BankInvoice.js
+++ react/components/Header/BankInvoice/BankInvoice.js
@@ -7,29 +7,25 @@ import PrinterIcon from '../../../Icons/PrinterIcon'
 import BarCode from './BarCode'
 import Embeded from './Embeded'
 
-const BankInvoice = ({ url, invoiceBarCodeNumber, intl }) => {
-  return (
-    <div className="pv4 w-80-ns w-90 center bb b--muted-5">
-      <p className="t-heading-4">
-        {intlMessage(intl, 'header.bankinvoice.header')}
-      </p>
-      <div className="flex justify-between items-center">
-        {invoiceBarCodeNumber && (
-          <BarCode barCodeNumber={invoiceBarCodeNumber} />
-        )}
-        <a href={url}>
-          <Button variation="secondary">
-            <div className="mr3">
-              <PrinterIcon />
-            </div>
-            {intlMessage(intl, 'header.bankinvoice.print')}
-          </Button>
-        </a>
-      </div>
-      <Embeded url={url} />
+const BankInvoice = ({ url, invoiceBarCodeNumber, intl }) => (
+  <div className="pv4 w-80-ns w-90 center bb b--muted-5">
+    <p className="t-heading-4">
+      {intlMessage(intl, 'header.bankinvoice.header')}
+    </p>
+    <div className="flex justify-between items-center">
+      {invoiceBarCodeNumber && <BarCode barCodeNumber={invoiceBarCodeNumber} />}
+      <a href={url}>
+        <Button variation="secondary">
+          <div className="mr3">
+            <PrinterIcon />
+          </div>
+          {intlMessage(intl, 'header.bankinvoice.print')}
+        </Button>
+      </a>
     </div>
-  )
-}
+    <Embeded url={url} />
+  </div>
+)
 
 BankInvoice.propTypes = {
   url: PropTypes.string,
--- react/components/Header/BankInvoice/BarCode.js
+++ react/components/Header/BankInvoice/BarCode.js
@@ -5,24 +5,22 @@ import Clipboard from 'react-clipboard.js'
 import { injectIntl, intlShape } from 'react-intl'
 import { intlMessage } from '../../../utils'
 
-const BarCode = ({ barCodeNumber, intl }) => {
-  return (
-    <div className="flex b--muted-4 ba br3 bw1">
-      <div className="tc ph9">
-        <p className="c-on-base">{barCodeNumber}</p>
-      </div>
-      <Clipboard
-        component="div"
-        data-clipboard-text={barCodeNumber}
-        className="b--muted-4 bl bw1 flex items-center"
-      >
-        <Button variation="tertiary">
-          {intlMessage(intl, 'header.bankinvoice.copy')}
-        </Button>
-      </Clipboard>
+const BarCode = ({ barCodeNumber, intl }) => (
+  <div className="flex b--muted-4 ba br3 bw1">
+    <div className="tc ph9">
+      <p className="c-on-base">{barCodeNumber}</p>
     </div>
-  )
-}
+    <Clipboard
+      component="div"
+      data-clipboard-text={barCodeNumber}
+      className="b--muted-4 bl bw1 flex items-center"
+    >
+      <Button variation="tertiary">
+        {intlMessage(intl, 'header.bankinvoice.copy')}
+      </Button>
+    </Clipboard>
+  </div>
+)
 
 BarCode.propTypes = {
   barCodeNumber: PropTypes.string,
