6f46db53b7bde21f640a926f480c5dce6b57e7ba
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-23 16:28:28 -0200

    Add __mocks__ folder to mock VTEX-IO packages
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ react/__mocks__/vtex.styleguide.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export function Button() {
+  return <div>Button</div>
+}
+
+export function ButtonWithIcon() {
+  return <div>ButtonWithIcon</div>
+}
+
+export function IconCaretDown() {
+  return <div>IconCaretDown</div>
+}
+
+export function IconCaretUp() {
+  return <div>IconCaretUp</div>
+}
+
+export function PageBlock() {
+  return <div data-testid="summary">PageBlock</div>
+}
+
+export function Alert() {
+  return <div>Alert</div>
+}
