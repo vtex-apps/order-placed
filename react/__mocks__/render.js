05a23a8f5cbe482391781a9639284a3d2f3f40ab
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-26 11:55:24 -0200

    Fix render mock
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- react/__mocks__/render.js
+++ react/__mocks__/render.js
@@ -1,3 +1,11 @@
-export function withRuntimeContext(Comp) {
-  return Comp
+import React, { Component } from 'react'
+
+export function withRuntimeContext(WrappedComponent) {
+  return class withRuntimeContext extends Component {
+    render() {
+      return (
+        <WrappedComponent {...this.props} runtime={{ account: 'vtexgame1' }} />
+      )
+    }
+  }
 }
