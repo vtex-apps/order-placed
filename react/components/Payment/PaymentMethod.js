9c22e09f7a2e1c91507b11a233f7894d525634a7
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-28 14:13:51 -0200

    Convert `PaymentMethod` to class component
    Add state to `PaymentMethod` so that additional info can be hidden

--- react/components/Payment/PaymentMethod.js
+++ react/components/Payment/PaymentMethod.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { intlShape, injectIntl } from 'react-intl'
-import { IconCaretDown } from 'vtex.styleguide'
+import { IconCaretDown, IconCaretUp } from 'vtex.styleguide'
 
 import { paymentShape } from '../../types'
 import { intlMessage } from '../../utils'
@@ -21,48 +21,58 @@ const paymentGroupSwitch = (payment, intl) => {
   }
 }
 
-const PaymentMethod = ({ payment, transactionId, intl }) => {
-  const isCreditCard = payment.group === 'creditCard'
-  const isBankInvoice = payment.group === 'bankInvoice'
+class PaymentMethod extends Component {
+  state = { open: false }
 
-  return (
-    <article className="flex justify-between items-center">
-      <div className="t-body lh-solid">
-        <p className="c-on-base">{paymentGroupSwitch(payment.group, intl)}</p>
-        {isCreditCard && (
+  handleClick = () => {
+    this.setState(prevState => ({ open: !prevState.open }))
+  }
+
+  render() {
+    const { payment, transactionId, intl } = this.props
+    const open = this.state.open
+    const isCreditCard = payment.group === 'creditCard'
+    const isBankInvoice = payment.group === 'bankInvoice'
+
+    return (
+      <article className="flex justify-between">
+        <div className="t-body lh-solid">
+          <p className="c-on-base">{paymentGroupSwitch(payment.group, intl)}</p>
+          {isCreditCard && (
+            <p className="c-muted-1">
+              {intlMessage(intl, 'payments.creditcard.lastDigits', {
+                lastDigits: payment.lastDigits,
+              })}
+            </p>
+          )}
           <p className="c-muted-1">
-            {intlMessage(intl, 'payments.creditcard.lastDigits', {
-              lastDigits: payment.lastDigits,
-            })}
+            <Price value={payment.value} />
+            {` ${intlMessage(intl, 'payments.installments', {
+              installments: payment.installments,
+            })}`}
           </p>
-        )}
-        <p className="c-muted-1">
-          <Price value={payment.value} />
-          {` ${intlMessage(intl, 'payments.installments', {
-            installments: payment.installments,
-          })}`}
-        </p>
-        {isBankInvoice && (
-          <ButtonLink url={payment.url} variation="primary">
-            {intlMessage(intl, 'payments.bankinvoice.print')}
-          </ButtonLink>
-        )}
-        <div>
-          <p className="c-muted-2">
-            {intlMessage(intl, 'payments.id', { id: payment.id })}
-          </p>
-          <p className="c-muted-2">
-            {intlMessage(intl, 'payments.transaction.id', {
-              id: transactionId,
-            })}
-          </p>
+          {isBankInvoice && (
+            <ButtonLink url={payment.url} variation="primary">
+              {intlMessage(intl, 'payments.bankinvoice.print')}
+            </ButtonLink>
+          )}
+          <div hidden={!open}>
+            <p className="c-muted-2">
+              {intlMessage(intl, 'payments.id', { id: payment.id })}
+            </p>
+            <p className="c-muted-2">
+              {intlMessage(intl, 'payments.transaction.id', {
+                id: transactionId,
+              })}
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="c-action-primary">
-        <IconCaretDown />
-      </div>
-    </article>
-  )
+        <div className="c-action-primary pt8" onClick={this.handleClick}>
+          {open ? <IconCaretUp /> : <IconCaretDown />}
+        </div>
+      </article>
+    )
+  }
 }
 
 PaymentMethod.propTypes = {
