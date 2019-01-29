4074d4da35fdb61bc51c52191ad9d2ee4b2b235f
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-29 16:03:24 -0200

    Fix conditional for rendering measurement units
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- react/components/ProductList/Product.js
+++ react/components/ProductList/Product.js
@@ -5,42 +5,49 @@ import { ProductImage } from 'vtex.order-details'
 import { intlMessage } from '../../utils'
 import FormattedPrice from '../Payment/FormattedPrice'
 
-const Product = ({ productInfo, intl }) => (
-  <article className="flex justify-between mv4 flex-column-s flex-row-m">
-    <div className="flex items-center flex-column flex-row-m">
-      <ProductImage
-        url={productInfo.imageUrl}
-        alt={productInfo.name}
-        className="w3 mr5"
-      />
-      <div className="flex flex-column items-between h-100">
-        <a
-          href={productInfo.detailUrl}
-          className="t-body c-muted-1 no-underline"
-          target="_blank"
-        >
-          <p>
-            {productInfo.name}
-            {productInfo.measurementUnit !== 'un' && (
-              <small className="t-mini c-on-base tc tl-m">
-                <br />
-                {`${productInfo.unitMultiplier} ${productInfo.measurementUnit}`}
-              </small>
-            )}
+const Product = ({ productInfo, intl }) => {
+  const showMeasurementUnit =
+    productInfo.unitMultiplier !== 1 || productInfo.measurementUnit !== 'un'
+
+  return (
+    <article className="flex justify-between mv4 flex-column-s flex-row-m">
+      <div className="flex items-center flex-column flex-row-m">
+        <ProductImage
+          url={productInfo.imageUrl}
+          alt={productInfo.name}
+          className="w3 mr5"
+        />
+        <div className="flex flex-column items-between h-100">
+          <a
+            href={productInfo.detailUrl}
+            className="t-body c-muted-1 no-underline"
+            target="_blank"
+          >
+            <p>
+              {productInfo.name}
+              {showMeasurementUnit && (
+                <small className="t-mini c-on-base tc tl-m">
+                  <br />
+                  {`${productInfo.unitMultiplier} ${
+                    productInfo.measurementUnit
+                  }`}
+                </small>
+              )}
+            </p>
+          </a>
+          <p className="t-mini c-muted-1 tc tl-m">
+            {intlMessage(intl, 'products.quantity', {
+              quantity: productInfo.quantity,
+            })}
           </p>
-        </a>
-        <p className="t-mini c-muted-1 tc tl-m">
-          {intlMessage(intl, 'products.quantity', {
-            quantity: productInfo.quantity,
-          })}
-        </p>
+        </div>
       </div>
-    </div>
-    <p className="tc tr-m">
-      <FormattedPrice value={productInfo.price * productInfo.quantity} />
-    </p>
-  </article>
-)
+      <p className="tc tr-m">
+        <FormattedPrice value={productInfo.price * productInfo.quantity} />
+      </p>
+    </article>
+  )
+}
 
 Product.propTypes = {
   productInfo: PropTypes.object.isRequired,
