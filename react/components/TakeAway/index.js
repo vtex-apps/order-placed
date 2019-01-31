5a8fbd2718d3f8bee21eb86b1e15482f10536536
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-31 11:14:08 -0200

    Layout adjustments

--- react/components/TakeAway/index.js
+++ react/components/TakeAway/index.js
@@ -6,7 +6,7 @@ import ProductList from '../ProductList'
 
 const TakeAway = ({ takeAwayPackages, intl }) => {
   return (
-    <section>
+    <section className="mv8 flex flex-column justify-between">
       <header>
         <p data-testid="shipping-header" className="t-heading-4-ns t-heading-5">
           {intl.formatMessage({ id: 'takeaway.header.title' })}
