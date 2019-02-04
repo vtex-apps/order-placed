37cd398af1458aa1d32e3bdb1c86b744f95537ab
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-04 18:22:52 -0200

    Add links to action buttons on OrderHeader

--- react/components/OrderInfo/OrderOptions.tsx
+++ react/components/OrderInfo/OrderOptions.tsx
@@ -1,12 +1,13 @@
 import React, { FunctionComponent } from 'react'
 import { InjectedIntlProps, injectIntl } from 'react-intl'
-import { Button } from 'vtex.styleguide'
+import ButtonLink from '../ButtonLink'
 
 interface Props {
   allowCancellation: boolean
   takeaway?: boolean
   className?: string
   fullWidth?: boolean
+  orderId?: string
 }
 
 const OrderOptions: FunctionComponent<Props & InjectedIntlProps> = ({
@@ -15,31 +16,54 @@ const OrderOptions: FunctionComponent<Props & InjectedIntlProps> = ({
   intl,
   className = '',
   fullWidth,
+  orderId,
 }) => (
   <div className={`${className} flex flex-wrap justify-between flex-nowrap-m`}>
     <div className="mr3-ns mb4-s mb0-m w-100 w-auto-m">
-      <Button variation="secondary" block={fullWidth}>
-        {takeaway
-          ? intl.formatMessage({
-              id: 'order.header.takeaway.printreceipt.button',
-            })
-          : intl.formatMessage({ id: 'order.header.update.button' })}
-      </Button>
+      {takeaway ? (
+        <ButtonLink variation="secondary" fullWidth={fullWidth} url="">
+          {intl.formatMessage({
+            id: 'order.header.takeaway.printreceipt.button',
+          })}
+        </ButtonLink>
+      ) : (
+        <ButtonLink
+          variation="secondary"
+          fullWidth={fullWidth}
+          url={`/account#/orders/${orderId}`}
+        >
+          {intl.formatMessage({ id: 'order.header.update.button' })}
+        </ButtonLink>
+      )}
     </div>
     {!takeaway && (
       <div className="mr3-ns mb4-s mb0-m w-100 w-auto-m">
-        <Button variation="secondary" block={fullWidth}>
+        <ButtonLink
+          variation="secondary"
+          fullWidth={fullWidth}
+          url="/account#/orders/"
+        >
           {intl.formatMessage({ id: 'order.header.myorders.button' })}
-        </Button>
+        </ButtonLink>
       </div>
     )}
     {allowCancellation && (
       <div className="w-100 w-auto-m">
-        <Button variation="danger-tertiary" block={fullWidth}>
-          {takeaway
-            ? intl.formatMessage({ id: 'order.header.takeaway.cancel.button' })
-            : intl.formatMessage({ id: 'order.header.cancel.button' })}
-        </Button>
+        {takeaway ? (
+          <ButtonLink variation="danger-tertiary" fullWidth={fullWidth} url="">
+            {intl.formatMessage({
+              id: 'order.header.takeaway.cancel.button',
+            })}
+          </ButtonLink>
+        ) : (
+          <ButtonLink
+            variation="danger-tertiary"
+            fullWidth={fullWidth}
+            url={`/account#/orders/${orderId}/cancel`}
+          >
+            {intl.formatMessage({ id: 'order.header.cancel.button' })}
+          </ButtonLink>
+        )}
       </div>
     )}
   </div>
