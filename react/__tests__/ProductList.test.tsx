087f521b40ea78b3ead5fe557141e4bfa6adea4f
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-09 13:14:25 -0200

    Add tests for attachments and services
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ react/__tests__/ProductList.test.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+import { render } from '../testUtils'
+
+import OrderInfo from '../components/OrderInfo'
+import { orderGroupQuery as serviceWithAttachment } from '../mocks/bundleServiceWithAttachment'
+import { orderGroupQuery as serviceWithNoAttachments } from '../mocks/bundleServiceWithNoAttachments'
+import { orderGroupQuery as serviceAndAttachment } from '../mocks/serviceAndAttachment'
+
+const getOrderFromOrderGroup = (orderGroup: OrderGroup, index: number) => {
+  return orderGroup.orders[index]
+}
+
+describe('Product List component', () => {
+  it('should render Attachment component if product has service or attachment', () => {
+    const orderInfo = getOrderFromOrderGroup(
+      serviceWithNoAttachments.orderGroup,
+      0
+    )
+    const { queryByText } = render(
+      <OrderInfo
+        order={orderInfo}
+        profile={orderInfo.clientProfileData}
+        numOfOrders={1}
+        index={0}
+      />
+    )
+
+    expect(queryByText('[TESTE QA]')).toBeDefined()
+  })
+
+  it('should render attachment from bundleItems item', () => {
+    const orderInfo = getOrderFromOrderGroup(
+      serviceWithAttachment.orderGroup,
+      0
+    )
+    const { queryByText } = render(
+      <OrderInfo
+        order={orderInfo}
+        profile={orderInfo.clientProfileData}
+        numOfOrders={1}
+        index={0}
+      />
+    )
+
+    expect(queryByText('This is a mock message')).toBeDefined()
+  })
+
+  it('should render attachments from item and also bundle items', () => {
+    const orderInfo = getOrderFromOrderGroup(
+      serviceAndAttachment.orderGroup,
+      0
+    )
+    const { queryByText } = render(
+      <OrderInfo
+        order={orderInfo}
+        profile={orderInfo.clientProfileData}
+        numOfOrders={1}
+        index={0}
+      />
+    )
+
+    expect(queryByText('[TESTE QA]')).toBeDefined()
+    expect(queryByText('1 semana')).toBeDefined()
+  })
+
+  it('should not render CarretDown and additional info for a bundle item that has no attachments', () => {
+    const orderInfo = getOrderFromOrderGroup(
+      serviceWithNoAttachments.orderGroup,
+      0
+    )
+    const { queryByTestId } = render(
+      <OrderInfo
+        order={orderInfo}
+        profile={orderInfo.clientProfileData}
+        numOfOrders={1}
+        index={0}
+      />
+    )
+
+    expect(queryByTestId('icon-caret-down')).toBeNull()
+  })
+})
