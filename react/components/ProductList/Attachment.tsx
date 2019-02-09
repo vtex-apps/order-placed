f8792d8003068552f850143fb4c9b7a96c68bc45
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-09 13:11:54 -0200

    Add new Attachment components and add support for bundle items and attachments
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ react/components/ProductList/Attachment.tsx
@@ -0,0 +1,77 @@
+import React, { Fragment, FunctionComponent, useState } from 'react'
+import { InjectedIntlProps, injectIntl } from 'react-intl'
+import { IconCaretDown, IconCaretUp } from 'vtex.styleguide'
+
+import { ProductImage } from 'vtex.order-details'
+import Price from '../Payment/FormattedPrice'
+
+interface Props {
+  attachmentsInfo: Attachment[]
+  bundleInfo: Bundle[]
+}
+
+const ProductAttachment: FunctionComponent<Props & InjectedIntlProps> = ({
+  bundleInfo,
+  attachmentsInfo,
+  intl,
+}) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <Fragment>
+      {bundleInfo &&
+        bundleInfo.map(bundleItem => (
+          <article className="bg-muted-5 pv3 ph5 br2 mv4">
+            <div className="flex justify-between">
+              {bundleItem.imageUrl && (
+                <ProductImage url={bundleItem.imageUrl} alt={bundleItem.name} />
+              )}
+              <p className="block c-on-base">{bundleItem.name}</p>
+              <div className="flex items-center">
+                <Price value={bundleItem.price} />
+                {bundleItem.attachments && bundleItem.attachments.length > 0 && (
+                  <div
+                    className="c-action-primary ml5"
+                    onClick={() => setIsOpen(!isOpen)}
+                  >
+                    {isOpen ? <IconCaretUp /> : <IconCaretDown />}
+                  </div>
+                )}
+              </div>
+            </div>
+            {bundleItem.attachments && bundleItem.attachments.length > 0 && (
+              <div hidden={!isOpen}>
+                <p className="c-muted-1">
+                  {bundleItem.attachments[0].content.text}
+                </p>
+              </div>
+            )}
+          </article>
+        ))}
+      {attachmentsInfo.length > 0 &&
+        attachmentsInfo.map(attachmentItem => (
+          <article className="bg-muted-5 pv3 ph5 br2 mv4">
+            <div className="flex justify-between">
+              <p className="block c-on-base">{attachmentItem.name}</p>
+              <div className="flex items-center">
+                <p className="mr5">
+                  {intl.formatMessage({ id: 'order.totals.pickup.free' })}
+                </p>
+                <div
+                  className="c-action-primary"
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  {isOpen ? <IconCaretUp /> : <IconCaretDown />}
+                </div>
+              </div>
+            </div>
+            <div hidden={!isOpen}>
+              <p className="c-muted-1">{attachmentItem.content.periodo}</p>
+            </div>
+          </article>
+        ))}
+    </Fragment>
+  )
+}
+
+export default injectIntl(ProductAttachment)
--- react/components/ProductList/index.tsx
+++ react/components/ProductList/index.tsx
@@ -1,15 +1,26 @@
 import React, { Fragment, FunctionComponent } from 'react'
+import ProductAttachment from './Attachment'
 import Product from './Product'
 
 interface Props {
   products: OrderItem[]
 }
 
-const ProductList: FunctionComponent<Props> = ({ products }) => (
-  <Fragment>
-    {products.map((product: OrderItem) => (
-      <Product productInfo={product} key={product.id} />
-    ))}
-  </Fragment>
-)
+const ProductList: FunctionComponent<Props> = ({ products }) => {
+  return (
+    <Fragment>
+      {products.map((product: OrderItem) => (
+        <Fragment key={product.id}>
+          <Product productInfo={product} key={product.id} />
+          {(product.bundleItems || product.attachments) && (
+            <ProductAttachment
+              attachmentsInfo={product.attachments}
+              bundleInfo={product.bundleItems}
+            />
+          )}
+        </Fragment>
+      ))}
+    </Fragment>
+  )
+}
 export default ProductList
