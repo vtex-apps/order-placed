e5eacce25e702cd968b902093c2277a992a9be37
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-11 17:24:06 -0200

    Add test for subscription items

--- react/__tests__/ProductList.test.tsx
+++ react/__tests__/ProductList.test.tsx
@@ -6,6 +6,7 @@ import OrderInfo from '../components/OrderInfo'
 import { orderGroupQuery as serviceWithAttachment } from '../mocks/bundleServiceWithAttachment'
 import { orderGroupQuery as serviceWithNoAttachments } from '../mocks/bundleServiceWithNoAttachments'
 import { orderGroupQuery as serviceAndAttachment } from '../mocks/serviceAndAttachment'
+import { orderGroupQuery as subscription } from '../mocks/subscriptions'
 
 const getOrderFromOrderGroup = (orderGroup: OrderGroup, index: number) => {
   return orderGroup.orders[index]
@@ -62,5 +63,21 @@ describe('Product List component', () => {
 
     expect(queryByText('[TESTE QA]')).toBeDefined()
     expect(queryByText('1 semana')).toBeDefined()
+  })
+
+  it('should render correct information and messages for an item with a subscription', () => {
+    const orderInfo = getOrderFromOrderGroup(subscription.orderGroup, 0)
+    const { queryByText } = render(
+      <OrderInfo
+        order={orderInfo}
+        profile={orderInfo.clientProfileData}
+        numOfOrders={1}
+        index={0}
+      />
+    )
+
+    expect(queryByText('Subscription')).toBeDefined()
+    expect(queryByText('Every 1 week')).toBeDefined()
+    expect(queryByText('Charged monthly at day 15'))
   })
 })
