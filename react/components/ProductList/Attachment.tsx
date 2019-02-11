37a07e0ae43adc6d6e65f9eeba090f05a9f95411
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-11 17:10:28 -0200

    Add support for handling items with subscriptions attached to them

--- react/components/ProductList/Attachment.tsx
+++ react/components/ProductList/Attachment.tsx
@@ -10,6 +10,66 @@ interface Props {
   bundleInfo: Bundle[]
 }
 
+const getSubscriptionInfo = (
+  attachmentItem: Attachment,
+  intl: ReactIntl.InjectedIntl
+) => {
+  const isSubscription = attachmentItem.name.indexOf('vtex.subscription') === 0
+  if (!isSubscription) {
+    return { isSubscription: false }
+  }
+  const vtexSubsPrefix = 'vtex.subscription.key.'
+  const subsFrequency: string =
+    attachmentItem.content[`${vtexSubsPrefix}frequency`]
+  const subsPurchaseDay =
+    attachmentItem.content[`${vtexSubsPrefix}purchaseday`]
+  const subsValidityBegin =
+    attachmentItem.content[`${vtexSubsPrefix}validity.begin`]
+  const subsValidityEnd =
+    attachmentItem.content[`${vtexSubsPrefix}validity.end`]
+
+  // This matches a key in the format: "3 months"
+  const numberPeriodRegex = /([\d]{1,3}) ([A-z]{1,20})/
+  // This matches a key in the format: "weekly"
+  const wordlyPeriodRegex = /([A-z]{1,20})/
+
+  const subsFrequencyString = numberPeriodRegex.test(subsFrequency)
+    ? intl.formatMessage(
+        {
+          id: `items.attachments.subscription.frequency.${
+            (numberPeriodRegex.exec(subsFrequency) || [])[2]
+          }`,
+        },
+        {
+          frequencyNumber: parseInt(
+            (numberPeriodRegex.exec(subsFrequency) || [])[1],
+            10
+          ),
+        }
+      )
+    : intl.formatMessage({
+        id: `items.attachments.subscription.frequency.${
+          (wordlyPeriodRegex.exec(subsFrequency) || [])[1]
+        }`,
+      })
+
+  const subsPurchaseDayString =
+    subsPurchaseDay !== ''
+      ? intl.formatMessage(
+          { id: 'items.attachments.subscription.purchaseday' },
+          { purchaseday: subsPurchaseDay }
+        )
+      : null
+
+  return {
+    isSubscription: true,
+    subsFrequency: subsFrequencyString,
+    subsPurchaseDay: subsPurchaseDayString,
+    subsValidityBegin,
+    subsValidityEnd,
+  }
+}
+
 const ProductAttachment: FunctionComponent<Props & InjectedIntlProps> = ({
   bundleInfo,
   attachmentsInfo,
@@ -73,42 +133,65 @@ const ProductAttachment: FunctionComponent<Props & InjectedIntlProps> = ({
           )
         })}
       {attachmentsInfo.length > 0 &&
-        attachmentsInfo.map(attachmentItem => (
-          <article
-            className="bg-muted-5 pv3 ph5 br2 mv4"
-            key={attachmentItem.name}
-          >
-            <div className="flex justify-between">
-              <p className="c-on-base">{attachmentItem.name}</p>
-              <div className="flex items-center">
-                <p className="mr5">
-                  {intl.formatMessage({ id: 'order.totals.pickup.free' })}
+        attachmentsInfo.map(attachmentItem => {
+          const {
+            isSubscription,
+            subsFrequency,
+            subsPurchaseDay,
+          } = getSubscriptionInfo(attachmentItem, intl)
+
+          return (
+            <article
+              className="bg-muted-5 pv3 ph5 br2 mv4"
+              key={attachmentItem.name}
+            >
+              <div className="flex justify-between">
+                <p className="c-on-base">
+                  {isSubscription
+                    ? intl.formatMessage({
+                        id: 'items.attachments.subscription',
+                      })
+                    : attachmentItem.name}
                 </p>
-                {attachmentItem.content && (
-                  <div
-                    className="c-action-primary"
-                    onClick={() => setIsOpen(!isOpen)}
-                  >
-                    {isOpen ? <IconCaretUp /> : <IconCaretDown />}
-                  </div>
-                )}
+                <div className="flex items-center">
+                  <p className="mr5">
+                    {intl.formatMessage({ id: 'order.totals.pickup.free' })}
+                  </p>
+                  {attachmentItem.content && (
+                    <div
+                      className="c-action-primary"
+                      onClick={() => setIsOpen(!isOpen)}
+                    >
+                      {isOpen ? <IconCaretUp /> : <IconCaretDown />}
+                    </div>
+                  )}
+                </div>
               </div>
-            </div>
-            {attachmentItem.content && (
-              <div hidden={!isOpen}>
-                {Object.keys(attachmentItem.content).map(key => {
-                  const contentLabel = key
-                  const contentValue = attachmentItem.content[key]
-                  return (
-                    <p className="c-muted-1" key={key}>
-                      {`${contentLabel}: ${contentValue}`}
-                    </p>
-                  )
-                })}
-              </div>
-            )}
-          </article>
-        ))}
+              {attachmentItem.content && (
+                <div hidden={!isOpen}>
+                  {isSubscription ? (
+                    <Fragment>
+                      <p className="c-muted-1">{subsFrequency}</p>
+                      {subsPurchaseDay && (
+                        <p className="c-muted-1">{subsPurchaseDay}</p>
+                      )}
+                    </Fragment>
+                  ) : (
+                    Object.keys(attachmentItem.content).map(key => {
+                      const contentLabel = key
+                      const contentValue = attachmentItem.content[key]
+                      return (
+                        <p className="c-muted-1" key={key}>
+                          {`${contentLabel}: ${contentValue}`}
+                        </p>
+                      )
+                    })
+                  )}
+                </div>
+              )}
+            </article>
+          )
+        })}
     </Fragment>
   )
 }
