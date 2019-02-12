e81c1f4f2525d06a39f7f864e76caace5ba7ec15
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-12 13:57:45 -0200

    Update base layout for `OrdeerTotal`

--- react/components/OrderInfo/OrderTotal.tsx
+++ react/components/OrderInfo/OrderTotal.tsx
@@ -19,29 +19,32 @@ const ShippingTotals: FunctionComponent<Props & InjectedIntlProps> = ({
   const totalsLine = 'flex justify-between items-center c-muted-1'
 
   return (
-    <div className="bg-muted-5 br2 pa5 mv9">
-      <div className={totalsLine}>
-        <p>
-          {intl.formatMessage({ id: 'order.totals.subtotal' }, { numItems })}
-        </p>
-        <Price value={totals[0].value} />
-      </div>
-      <div className={totalsLine}>
-        <p>{intl.formatMessage({ id: 'order.totals.delivery' })}</p>
-        <Price value={totals[2].value} />
-      </div>
-      <div className={totalsLine}>
-        <p>{intl.formatMessage({ id: 'order.totals.pickup' })}</p>
-        <p>{intl.formatMessage({ id: 'order.totals.pickup.free' })}</p>
-      </div>
-      <hr className="bg-muted-4 bt b--muted-4" />
-      <div className="flex justify-between items-center c-on-base">
-        <p>
-          <strong>{intl.formatMessage({ id: 'order.totals.total' })}</strong>
-        </p>
-        <strong>
-          <Price value={orderValue} />
-        </strong>
+    <div className="flex flex-row justify-end w-100">
+      <div className="br2 pa5 mv9 w-70 ">
+        <div className={totalsLine}>
+          <p className="mv3">
+            {intl.formatMessage({ id: 'order.totals.subtotal' }, { numItems })}
+          </p>
+          <div className="c-on-base">
+            <Price value={totals[0].value} />
+          </div>
+        </div>
+        <div className={totalsLine}>
+          <p className="mv3">
+            {intl.formatMessage({ id: 'order.totals.delivery' })}
+          </p>
+          <div className="c-on-base">
+            <Price value={totals[2].value} />
+          </div>
+        </div>
+        <div className="flex justify-between items-center c-on-base">
+          <p className="t-heading-2-ns t-heading-3 mv4">
+            <strong>{intl.formatMessage({ id: 'order.totals.total' })}</strong>
+          </p>
+          <strong className="t-heading-2-ns t-heading-3 mv4">
+            <Price value={orderValue} />
+          </strong>
+        </div>
       </div>
     </div>
   )
