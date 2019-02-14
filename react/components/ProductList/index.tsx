5fc397d000107a34d75bb560c453f01e1edc95b7
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-14 12:03:59 -0200

    Fix border being displayed between Product and Attachment

--- react/components/ProductList/index.tsx
+++ react/components/ProductList/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, FunctionComponent } from 'react'
+import React, { FunctionComponent } from 'react'
 import ProductAttachment from './Attachment'
 import Product from './Product'
 
@@ -10,18 +10,20 @@ const ProductList: FunctionComponent<Props> = ({ products }) => {
   return (
     <div className="flex flex-column justify-between w-100 w-75-m">
       {products.map((product: OrderItem, index: number) => (
-        <Fragment key={product.id}>
-          <Product
-            productInfo={product}
-            useBorder={index !== products.length - 1}
-          />
+        <article
+          className={`${
+            index !== products.length - 1 ? 'bb b--muted-5 pb4' : ''
+          }`}
+          key={product.id}
+        >
+          <Product productInfo={product} />
           {(product.bundleItems || product.attachments) && (
             <ProductAttachment
               attachmentsInfo={product.attachments}
               bundleInfo={product.bundleItems}
             />
           )}
-        </Fragment>
+        </article>
       ))}
     </div>
   )
