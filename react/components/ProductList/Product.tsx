af7d21f6a2d506cd8ec7b533e115b02dd9038920
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-18 19:15:40 -0300

    Remove unused variable from `Product` component

--- react/components/ProductList/Product.tsx
+++ react/components/ProductList/Product.tsx
@@ -16,20 +16,16 @@ const Product: FunctionComponent<Props & InjectedIntlProps> = ({
     productInfo.unitMultiplier !== 1 || productInfo.measurementUnit !== 'un'
 
   return (
-    <article
-      className="flex justify-between flex-column-s flex-row-m pv6"
-    >
+    <article className="flex justify-between flex-column-s flex-row-m pv6">
       <div className="flex items-center flex-column flex-row-m mr8-m">
         <div className="w4 mr5">
           <ProductImage
-          url={productInfo.imageUrl}
-          alt={productInfo.name}
-          className="w4 mr6"
-        />
+            url={productInfo.imageUrl}
+            alt={productInfo.name}
+            className="w4 mr6"
+          />
         </div>
-        <div className={`flex flex-column items-between justify-between h-100 ${
-        useBorder ? 'bb b--muted-4 pb4' : ''
-      }`}>
+        <div className="flex flex-column items-between justify-between h-100">
           <a
             href={productInfo.detailUrl}
             className="t-body c-muted-1 no-underline"
@@ -60,7 +56,7 @@ const Product: FunctionComponent<Props & InjectedIntlProps> = ({
       <p className="tc tr-m">
         <FormattedPrice value={productInfo.price * productInfo.quantity} />
       </p>
-    </div>
+    </article>
   )
 }
 
