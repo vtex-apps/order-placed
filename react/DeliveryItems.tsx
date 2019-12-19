437b8bc89f62374d2e3bd643e0895f0f1c4410e6
Christian Kaisermann <christian@kaisermann.me>
2019-12-19 17:36:38 -0300

    Add big blocks for item listing

---
+++ react/DeliveryItems.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react'
+
+import ProductList from './components/ProductList'
+import ShippingHeader from './components/Shipping/ShippingHeader'
+import { useOrder } from './components/OrderContext'
+
+const Shipping: FC = () => {
+  const { deliveryParcels, giftRegistryData } = useOrder()
+
+  if (deliveryParcels.length === 0) {
+    return null
+  }
+
+  return (
+    <section className="bb b--muted-4">
+      {deliveryParcels.map((deliveryParcel, index) => (
+        <div
+          className="mv8 flex-l justify-between flex-column flex-row-m"
+          key={index}
+        >
+          <ShippingHeader
+            shippingData={deliveryParcel}
+            index={index}
+            numPackages={deliveryParcels.length}
+            giftRegistry={giftRegistryData}
+          />
+          <ProductList products={deliveryParcel.items} />
+        </div>
+      ))}
+    </section>
+  )
+}
+export default Shipping
