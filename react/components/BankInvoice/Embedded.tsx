8a835762b82c2ca270f958c9c9caaa039cbe3747
Christian Kaisermann <christian@kaisermann.me>
2019-12-23 18:19:51 -0300

    Fix some style issues

--- react/components/BankInvoice/Embedded.tsx
+++ react/components/BankInvoice/Embedded.tsx
@@ -1,56 +1,45 @@
-import React, { Component } from 'react'
+import React, { FC, useState } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { ButtonWithIcon, IconCaretDown, IconCaretUp } from 'vtex.styleguide'
-
-class Embedded extends Component<Props> {
-  public state = { open: false }
-
-  public handleClick = () => {
-    this.setState((prevState: State) => ({ open: !prevState.open }))
-  }
-
-  public render() {
-    const open = this.state.open
-    const { url } = this.props
-
-    return (
-      <div className="flex flex-column b--muted-4 ba br3 bw1">
-        <div className="bg-muted-1 flex justify-center">
-          <embed
-            width="80%"
-            height={open ? 900 : 300}
-            src={url}
-            data-testid="embedded-bank-invoice"
-          />
-        </div>
-        {open ? (
-          <ButtonWithIcon
-            icon={<IconCaretUp />}
-            variation="tertiary"
-            onClick={this.handleClick}
-          >
-            <FormattedMessage id="store/header.bankinvoice.embeded.collapse" />
-          </ButtonWithIcon>
-        ) : (
-          <ButtonWithIcon
-            icon={<IconCaretDown />}
-            variation="tertiary"
-            onClick={this.handleClick}
-          >
-            <FormattedMessage id="store/header.bankinvoice.embeded.expand" />
-          </ButtonWithIcon>
-        )}
-      </div>
-    )
-  }
-}
 
 interface Props {
   url: string
 }
 
-interface State {
-  open: boolean
+const Embedded: FC<Props> = ({ url }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className="b--muted-4 ba br3 bw1">
+      <div className="bg-muted-1">
+        <embed
+          width="100%"
+          height={isOpen ? 900 : 300}
+          src={url}
+          data-testid="embedded-bank-invoice"
+        />
+      </div>
+      {isOpen ? (
+        <ButtonWithIcon
+          icon={<IconCaretUp />}
+          variation="tertiary"
+          onClick={() => setIsOpen(false)}
+          block
+        >
+          <FormattedMessage id="store/header.bankinvoice.embeded.collapse" />
+        </ButtonWithIcon>
+      ) : (
+        <ButtonWithIcon
+          icon={<IconCaretDown />}
+          variation="tertiary"
+          onClick={() => setIsOpen(true)}
+          block
+        >
+          <FormattedMessage id="store/header.bankinvoice.embeded.expand" />
+        </ButtonWithIcon>
+      )}
+    </div>
+  )
 }
 
 export default Embedded
