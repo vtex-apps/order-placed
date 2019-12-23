9e701bd27afd4896b567bc17ccf9e9cc323a8f7c
Christian Kaisermann <christian@kaisermann.me>
2019-12-23 17:11:46 -0300

    Fix payment method prop and loose articles

--- react/OrderPayment.tsx
+++ react/OrderPayment.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { PaymentMethod } from 'vtex.order-details'
 
 import { useOrder } from './components/OrderContext'
+import { useCurrency } from './components/CurrencyContext'
 
 const OrderPayment = () => {
   const order = useOrder()
+  const currency = useCurrency()
 
   const { payments, transactionId } = order.paymentData.transactions[0]
 
@@ -12,7 +14,11 @@ const OrderPayment = () => {
     <div className="flex flex-column flex-row-m">
       {payments.map((payment, idx) => (
         <div key={idx} className="pb8-s mr9-m">
-          <PaymentMethod payment={payment} transactionId={transactionId} />
+          <PaymentMethod
+            payment={payment}
+            transactionId={transactionId}
+            currency={currency}
+          />
         </div>
       ))}
     </div>
