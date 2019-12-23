2044328adcb9e73762f15b6e9e6d8e1a1bdd28ea
Christian Kaisermann <christian@kaisermann.me>
2019-12-23 19:00:00 -0300

    Refactor summary markup

--- react/OrderPlacedSummary.tsx
+++ react/OrderPlacedSummary.tsx
@@ -34,73 +34,73 @@ const OrderSummary: FC = () => {
       className="bb b--muted-4 mb9 pb9 w-90 w-80-ns center flex-m justify-between"
     >
       <div className="ba b--muted-5 bw1 br3 w-50-m w-100 mr4-m mb4 mb0-m">
-        <p className="t-heading-5 tc bb b--muted-5 bw1 pb5 c-on-base">
+        <div className="pv5 t-heading-5 tc bb b--muted-5 bw1 c-on-base">
           <FormattedMessage id="store/summary.shipping" />
-        </p>
-        <p className="mb0 t-body ph6 mt6">
-          <strong>
+        </div>
+        <div className="pa6 t-body">
+          <div className="mb4">
+            <strong>
+              <FormattedMessage
+                id="store/summary.items"
+                values={{ itemsQuantity: deliveryItemsQuantity }}
+              />
+            </strong>
             <FormattedMessage
-              id="store/summary.items"
+              id="store/summary.shipping.quantity"
+              values={{ shippings: totalDeliveryParcels.length }}
+            />
+          </div>
+          <div className="t-heading-5 mb8">
+            <TranslateEstimate
+              shippingEstimate={longestDeliveryEstimate.shippingEstimate}
+            />
+          </div>
+          <div className="c-muted-2 lh-copy">
+            <FormattedMessage
+              id="store/summary.shipping.address"
               values={{
-                itemsQuantity: deliveryItemsQuantity,
+                addressNumber: totalDeliveryParcels[0].address.number,
+                addressStreet: totalDeliveryParcels[0].address.street,
               }}
             />
-          </strong>
-          <FormattedMessage
-            id="store/summary.shipping.quantity"
-            values={{
-              shippings: totalDeliveryParcels.length,
-            }}
-          />
-        </p>
-        <p className="t-heading-5 mt4 mb8 ph6">
-          <TranslateEstimate
-            shippingEstimate={longestDeliveryEstimate.shippingEstimate}
-          />
-        </p>
-        <p className="c-muted-2 self-baseline mv6 lh-copy ph6">
-          <FormattedMessage
-            id="store/summary.shipping.address"
-            values={{
-              addressNumber: totalDeliveryParcels[0].address.number,
-              addressStreet: totalDeliveryParcels[0].address.street,
-            }}
-          />
-        </p>
+          </div>
+        </div>
       </div>
       <div className="ba b--muted-5 bw1 br3 w-50-m w-100 ml4-m mt4 mt0-m">
-        <p className="t-heading-5 tc bb b--muted-5 bw1 pb5 c-on-base">
+        <div className="pv5 t-heading-5 tc bb b--muted-5 bw1 c-on-base">
           <FormattedMessage id="store/summary.pickup" />
-        </p>
-        <p className="mb0 t-body ph6 mt6">
-          <strong>
+        </div>
+        <div className="pa6 t-body">
+          <div className="mb4">
+            <strong>
+              <FormattedMessage
+                id="store/summary.items"
+                values={{
+                  itemsQuantity: pickUpItemsQuantity,
+                }}
+              />
+            </strong>
             <FormattedMessage
-              id="store/summary.items"
+              id="store/summary.pickup.quantity"
               values={{
-                itemsQuantity: pickUpItemsQuantity,
+                pickups: totalPickUpParcels.length,
               }}
             />
-          </strong>
-          <FormattedMessage
-            id="store/summary.pickup.quantity"
-            values={{
-              pickups: totalPickUpParcels.length,
-            }}
-          />
-        </p>
-        <p className="t-heading-5 mt4 mb8 ph6">
-          <TranslateEstimate
-            shippingEstimate={longestPickUpEstimate.shippingEstimate}
-          />
-        </p>
-        <p className="c-muted-2 self-baseline mv6 lh-copy ph6">
-          <FormattedMessage
-            id="store/summary.pickup.friendlyName"
-            values={{
-              friendlyName: totalPickUpParcels[0].pickupFriendlyName,
-            }}
-          />
-        </p>
+          </div>
+          <div className="t-heading-5 mb8">
+            <TranslateEstimate
+              shippingEstimate={longestPickUpEstimate.shippingEstimate}
+            />
+          </div>
+          <div className="c-muted-2 lh-copy">
+            <FormattedMessage
+              id="store/summary.pickup.friendlyName"
+              values={{
+                friendlyName: totalPickUpParcels[0].pickupFriendlyName,
+              }}
+            />
+          </div>
+        </div>
       </div>
     </section>
   )
