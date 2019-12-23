225c2bb5017550b16a5b84da748b3c66f1ce936b
Christian Kaisermann <christian@kaisermann.me>
2019-12-23 18:19:51 -0300

    Fix some style issues

--- react/OrderTotal.tsx
+++ react/OrderTotal.tsx
@@ -13,8 +13,8 @@ const OrderTotal: FC = () => {
     <div className="flex-l justify-end w-100">
       <ul className="list pa0 mv8 w-100 w-60-l c-muted-1">
         {totals.map(total => (
-          <li className="flex justify-between items-center" key={total.id}>
-            <span className="mv3">
+          <li className="pv3 flex justify-between items-center" key={total.id}>
+            <span>
               <TranslateTotalizer totalizer={total} />
               {total.id === 'Items' && ` (${numItems})`}
             </span>
@@ -23,7 +23,7 @@ const OrderTotal: FC = () => {
             </span>
           </li>
         ))}
-        <li className="flex justify-between items-center c-on-base t-heading-2-ns t-heading-3 mv4">
+        <li className="pv3 flex justify-between items-center c-on-base t-heading-2-ns t-heading-3">
           <strong>
             <FormattedMessage id="store/order.totals.total" />
           </strong>
