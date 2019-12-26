3c2fc92da3710abcd24a00f87937f6cd084edb0c
Christian Kaisermann <christian@kaisermann.me>
2019-12-26 18:30:17 -0300

    Remove unused withoutssr component

--- react/typings/render.d.ts
+++ react/typings/render.d.ts
@@ -10,7 +10,6 @@ declare module 'vtex.render-runtime' {
   }
 
   export const Link: any
-  export const NoSSR: any
   export const RenderContextConsumer: ReactElement
   export const canUseDOM: boolean
   export const withRuntimeContext: any
