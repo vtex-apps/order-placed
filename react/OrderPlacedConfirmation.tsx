3637bd44df3d0c7db9c1ff897cf2214970e01d0b
Christian Kaisermann <christian@kaisermann.me>
2020-01-03 15:02:11 -0300

    Add borderless prop to orderSection

--- react/OrderPlacedConfirmation.tsx
+++ react/OrderPlacedConfirmation.tsx
@@ -4,6 +4,7 @@ import { Button } from 'vtex.styleguide'
 
 import SuccessIcon from './icons/Success'
 import { useOrderGroup } from './components/OrderGroupContext'
+import OrderSection from './OrderSection'
 
 const OrderPlacedConfirmation: FC = () => {
   const orderGroup = useOrderGroup()
@@ -12,33 +13,32 @@ const OrderPlacedConfirmation: FC = () => {
   const hasDelivery = orderGroup.totalDeliveryParcels.length > 0
   const hasPickUp = orderGroup.totalPickUpParcels.length > 0
   const hasTakeAway = orderGroup.totalTakeAwayParcels.length > 0
-  const onlyTakeAway = !hasDelivery && !hasPickUp && hasTakeAway
 
   return (
-    <section className={onlyTakeAway ? 'bb b--muted-4 pb6' : ''}>
-      <div className="flex justify-center c-success">
-        <SuccessIcon size={50} />
-      </div>
-      <p className="tc c-on-base mt7 mb0 t-heading-4 w-90 center">
-        <FormattedMessage id="store/header.thanks" />
-      </p>
-      <p className="center mt5 t-body tc c-muted-1 lh-copy w-90 center">
-        <FormattedMessage
-          id="store/header.email"
-          values={{
-            lineBreak: <br />,
-            userEmail: <strong className="nowrap">{profile.email}</strong>,
-          }}
-        />
-      </p>
-      <div className="flex justify-center t-action mv8 w-90 center">
-        <div className="tr c-action-primary">
+    <OrderSection borderless={(hasDelivery || hasPickUp) && !hasTakeAway}>
+      <div className="tc w-90 center">
+        <div className="flex justify-center c-success">
+          <SuccessIcon size={50} />
+        </div>
+        <h4 className="tc c-on-base mt7 mb0 t-heading-4">
+          <FormattedMessage id="store/header.thanks" />
+        </h4>
+        <p className="mt5 t-body tc c-muted-1 lh-copy ">
+          <FormattedMessage
+            id="store/header.email"
+            values={{
+              lineBreak: <br />,
+              userEmail: <strong className="nowrap">{profile.email}</strong>,
+            }}
+          />
+        </p>
+        <div className="mv8">
           <Button variation="secondary" onClick={() => window.print()}>
             <FormattedMessage id="store/header.print.button" />
           </Button>
         </div>
       </div>
-    </section>
+    </OrderSection>
   )
 }
 export default OrderPlacedConfirmation
