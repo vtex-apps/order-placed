19422556de01810356a0edbfe84e3b351d370a97
Christian Kaisermann <christian@kaisermann.me>
2020-01-03 15:02:11 -0300

    Add borderless prop to orderSection

--- react/OrderSection.tsx
+++ react/OrderSection.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from 'react'
 
-const OrderSection: FC = ({ children }) => {
-  return <section className="bb b--muted-4">{children}</section>
+interface Props {
+  borderless?: boolean
+}
+
+const OrderSection: FC<Props> = ({ children, borderless = false }) => {
+  return (
+    <section className={`${borderless ? '' : 'bb b--muted-4'}`}>
+      {children}
+    </section>
+  )
 }
 
 export default OrderSection
