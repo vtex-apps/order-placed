4a32a6a78b71050e4da48c10d187109be02be2f4
Christian Kaisermann <christian@kaisermann.me>
2020-01-06 17:29:19 -0300

    Refactor attachment components

---
+++ react/components/ProductList/Attachments.tsx
@@ -0,0 +1,56 @@
+import React, { FC, Fragment, ReactNode } from 'react'
+import { FormattedMessage, useIntl } from 'react-intl'
+
+import { isSubscription, getSubscriptionInfo } from '../../utils'
+import AttachmentAccordion from './AttachmentAccordion'
+
+interface Props {
+  product: OrderItem
+}
+
+const ProductAttachment: FC<Props> = ({ product }) => {
+  const intl = useIntl()
+  const { attachments } = product
+
+  if (!attachments?.length) {
+    return null
+  }
+
+  return (
+    <Fragment>
+      {attachments.map(attachmentItem => {
+        let content: string[]
+        let titleLabel: string | ReactNode
+        let toggleLabel: string | ReactNode
+
+        if (isSubscription(attachmentItem)) {
+          const { subsFrequency, subsPurchaseDay } = getSubscriptionInfo(
+            attachmentItem,
+            intl
+          )
+          titleLabel = (
+            <FormattedMessage id="store/items.attachments.subscription" />
+          )
+          content = [subsFrequency, subsPurchaseDay]
+        } else {
+          titleLabel = attachmentItem.name
+          toggleLabel = <FormattedMessage id="store/order.totals.pickup.free" />
+          content = Object.keys(attachmentItem.content).map(
+            key => `${key}: ${attachmentItem.content[key]}`
+          )
+        }
+
+        return (
+          <AttachmentAccordion
+            key={attachmentItem.name}
+            titleLabel={titleLabel}
+            toggleLabel={toggleLabel}
+            content={content}
+          />
+        )
+      })}
+    </Fragment>
+  )
+}
+
+export default ProductAttachment
