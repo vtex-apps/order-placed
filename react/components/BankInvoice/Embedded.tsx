7a9473b89044584fc781d195b82519ebdcb1a310
Christian Kaisermann <christian@kaisermann.me>
2020-01-06 11:11:12 -0300

    Add css handles to bank invoice

--- react/components/BankInvoice/Embedded.tsx
+++ react/components/BankInvoice/Embedded.tsx
@@ -1,25 +1,33 @@
 import React, { FC, useState } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { ButtonWithIcon, IconCaretDown, IconCaretUp } from 'vtex.styleguide'
+import { useCssHandles } from 'vtex.css-handles'
+
+const CSS_HANDLES = [
+  'bankInvoiceEmbedWrapper',
+  'bankInvoiceEmbedBackground',
+  'bankInvoiceEmbed',
+]
 
 interface Props {
   url: string
 }
 
 const Embedded: FC<Props> = ({ url }) => {
+  const handles = useCssHandles(CSS_HANDLES)
   const [isOpen, setIsOpen] = useState(false)
 
   const toggle = () => setIsOpen(!isOpen)
 
   return (
-    <div className="b--muted-4 ba br3 bw1">
-      <div className="bg-muted-1">
+    <div className={`${handles.bankInvoiceEmbedWrapper} b--muted-4 ba br3 bw1`}>
+      <div className={`${handles.bankInvoiceEmbedBackground} bg-muted-1`}>
         <embed
           width="80%"
           height={isOpen ? 900 : 300}
           src={url}
           data-testid="embedded-bank-invoice"
-          className="db center"
+          className={`${handles.bankInvoiceEmbed} db center`}
         />
       </div>
       {isOpen ? (
