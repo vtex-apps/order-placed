92c2de44782798fff7ef0740010bce91dedc4b90
Christian Kaisermann <christian@kaisermann.me>
2020-01-06 11:22:27 -0300

    Document css handles

--- react/PickupItems.tsx
+++ react/PickupItems.tsx
@@ -17,7 +17,7 @@ const PickupItems: FC = () => {
   }
 
   return (
-    <OrderSection id="pickup-items">
+    <OrderSection id="pickupItems">
       {pickUpParcels.map((pickup: Parcel, index: number) => (
         <div
           className={`${applyModifiers(
