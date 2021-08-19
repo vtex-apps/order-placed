750209921d373aeec353d26b7cc18d77b3027818
bhuvana-lingam <bhuvaneswari@trikatechnologies.com>
2021-08-19 17:27:19 +0530

    Fix to show product price after discount for each item in the list

--- react/components/ProductList/Product.tsx
+++ react/components/ProductList/Product.tsx
@@ -27,14 +27,14 @@ const Product: FC<Props> = ({ product }) => {
     imageUrl,
     measurementUnit,
     name,
-    price,
+    sellingPrice,
     quantity,
     unitMultiplier,
     isGift,
   } = product
   const handles = useCssHandles(CSS_HANDLES)
   const showMeasurementUnit = unitMultiplier !== 1 || measurementUnit !== 'un'
-  const productSubtotal = price * quantity * unitMultiplier
+  const productSubtotal = sellingPrice * quantity * unitMultiplier
 
   return (
     <div className={`${handles.productWrapper} w-100 flex-m tc tl-m`}>
