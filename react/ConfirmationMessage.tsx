0861a3fca503a43ba278fa09c9b5e0501d945a49
Estacio Pereira <estacio.pereira@vtex.com.br>
2021-09-03 11:56:06 -0300

    Changes needed to make the carbon-free hackathon idea to work. Add carbon-free shipping static summary to order placed

--- react/ConfirmationMessage.tsx
+++ react/ConfirmationMessage.tsx
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 
 import { useOrderGroup } from './components/OrderGroupContext'
+import GreenShippingIcon from './Icons/GreenShippingIcon'
 
 const CSS_HANDLES = ['confirmationMessage']
 
@@ -12,17 +13,30 @@ const ConfirmationMessage: FC = () => {
   const profile = orderGroup.orders[0].clientProfileData
 
   return (
-    <p
-      className={`${handles.confirmationMessage} mt5 t-body tc c-muted-1 lh-copy`}
-    >
-      <FormattedMessage
-        id="store/header.email"
-        values={{
-          lineBreak: <br />,
-          userEmail: <strong className="nowrap">{profile.email}</strong>,
-        }}
-      />
-    </p>
+    <>
+      <p
+        className={`${handles.confirmationMessage} mt5 t-body tc c-muted-1 lh-copy`}
+      >
+        <FormattedMessage
+          id="store/header.email"
+          values={{
+            lineBreak: <br />,
+            userEmail: <strong className="nowrap">{profile.email}</strong>,
+          }}
+        />
+      </p>
+      <div className="mt5 flex-column items-center tc">
+        <GreenShippingIcon />
+        <p className="mt3 c-muted-1 t-heading-6">
+          You offset <span className="c-success">102.7kg</span> carbon with this
+          purchase
+        </p>
+        <p className="mt3 c-muted-1 t-small">
+          <span className="c-link pointer">Click here</span> for more
+          information on how you contributed to the environment.
+        </p>
+      </div>
+    </>
   )
 }
 export default ConfirmationMessage
