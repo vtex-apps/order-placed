2d9659daf22edc13c7aa0cd9c2f25f10e0f4f8df
Segopotso Makhutja <leketi.s@gmail.com>
2023-04-02 16:57:49 +0200

    refactor and extract furniture note to own component

--- react/ConfirmationMessage.tsx
+++ react/ConfirmationMessage.tsx
@@ -3,54 +3,56 @@ import { FormattedMessage } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 
 import { useOrderGroup } from './components/OrderGroupContext'
+import { FurnitureNote } from './FurnitureNote'
 
-const CSS_HANDLES = ['confirmationMessage', 'furnitureNote']
+const CSS_HANDLES = ['confirmationMessage']
 
 const ConfirmationMessage: FC = () => {
   const handles = useCssHandles(CSS_HANDLES)
   const orderGroup = useOrderGroup()
 
-  return (
-    <div className='container'>
-        <p
-          className={`${handles.confirmationMessage} mt5 t-body tc c-muted-1 lh-copy`}
-        >
-          {orderGroup.orders[0].clientProfileData.customerEmail ? (
-            <FormattedMessage
-              id="store/header.email"
-              values={{
-                lineBreak: <br />,
-                userEmail: (
-                  <strong className="nowrap">
-                    {orderGroup.orders[0].clientProfileData.customerEmail}
-                  </strong>
-                ),
-              }}
-            />
-          ) : (
-            <FormattedMessage
-              id="store/header.phone"
-              values={{
-                lineBreak: <br />,
-                phone: (
-                  <strong className="nowrap">
-                    {orderGroup.orders[0].clientProfileData.phone}
-                  </strong>
-                ),
-              }}
-            />
-          )}
-        </p>
+  const shippingMethod =
+    orderGroup.orders[0].pickUpParcels.length > 0 ? 'collect' : 'deliver'
 
-        {/* furniture message */}
-        <div className={`${handles.furnitureNote}`}>
-          <h3> furniture delivery </h3>
-          <p>
-            We’ll call the recipient in the next few days to arrange the furniture delivery.
-            <br/> Please ensure sufficient space to receive the goods and keep in mind that the couriers can’t hoist goods onto balconies.
-            <br/> <a href='#'>Frequently asked questions</a>
-          </p>
-        </div>
+  const shippingValue =
+    // eslint-disable-next-line prettier/prettier
+    orderGroup.orders[0].totals.find((item) => item.id === 'Shipping')?.value ??
+    0
+
+  const hasFurniture = orderGroup.orders[0].value > 50000 && shippingValue > 0
+
+  return (
+    <div className={`${handles.confirmationContainer}`}>
+      <p
+        className={`${handles.confirmationMessage} mt5 t-body tc c-muted-1 lh-copy`}
+      >
+        {orderGroup.orders[0].clientProfileData.customerEmail ? (
+          <FormattedMessage
+            id="store/header.email"
+            values={{
+              lineBreak: <br />,
+              userEmail: (
+                <strong className="nowrap">
+                  {orderGroup.orders[0].clientProfileData.customerEmail}
+                </strong>
+              ),
+            }}
+          />
+        ) : (
+          <FormattedMessage
+            id="store/header.phone"
+            values={{
+              lineBreak: <br />,
+              phone: (
+                <strong className="nowrap">
+                  {orderGroup.orders[0].clientProfileData.phone}
+                </strong>
+              ),
+            }}
+          />
+        )}
+      </p>
+      {hasFurniture ? <FurnitureNote shippingMethod={shippingMethod} /> : null}
     </div>
   )
 }
---
+++ react/FurnitureNote.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useCssHandles } from 'vtex.css-handles'
+
+interface FurnitureNoteProps {
+  shippingMethod: string
+}
+
+const CSS_HANDLES = [
+  'confirmationMessage',
+  'furnitureNote',
+  'confirmationContainer',
+  'noteLink',
+  'noteTitle',
+]
+
+const deliveryMessage = `
+  we’ll call the recipient in the next few days to arrange the furniture
+  delivery.
+  <br /> Please ensure sufficient space to receive the goods and keep in
+  mind that the couriers can’t hoist goods onto balconies.
+  <br />
+`
+
+const collectMessage = `
+  We’ll call the recipient to book a collection time with the warehouse.
+`
+
+export const FurnitureNote = ({ shippingMethod }: FurnitureNoteProps) => {
+  const handles = useCssHandles(CSS_HANDLES)
+
+  return (
+    <div className={`${handles.furnitureNote}`}>
+      <h4 className={`${handles.noteTitle}`}>furniture delivery</h4>
+      <p>
+        {shippingMethod === 'deliver' ? deliveryMessage : collectMessage}
+        <br />{' '}
+      </p>
+      <a href="#/customer-service/help" className={handles.noteLink}>
+        Frequently asked questions
+      </a>
+    </div>
+  )
+}
