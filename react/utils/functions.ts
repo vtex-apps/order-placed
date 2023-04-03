77989e676ddb4beada090d901efb87887027a6b0
Segopotso Makhutja <leketi.s@gmail.com>
2023-04-03 08:40:47 +0200

    remove unused util function

--- react/utils/functions.ts
+++ react/utils/functions.ts
@@ -12,19 +12,3 @@ export const getEndpoints = (account: string): Endpoints => {
     clientsApiUrl: `${masterData}clients`,
   }
 }
-
-interface CheckFurnitureProps {
-  orderValue: number
-  shippingFee: number | string
-}
-
-export const hasFurniture = ({
-  orderValue,
-  shippingFee,
-}: CheckFurnitureProps) => {
-  // If order > R500 and there’s shipping fee === furniture
-  // If it’s not free and/or R50 === furniture
-  // R500 in cents = 50000
-  if (orderValue > 50000 && shippingFee) return true
-  return false
-}
