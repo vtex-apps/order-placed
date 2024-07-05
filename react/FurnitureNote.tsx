5fd4b485cc1d3ae1da0c306634c43a1a4af8b298
Segopotso Makhutja <leketi.s@gmail.com>
2024-07-05 10:28:07 +0200

    remove dangerously set html

--- react/FurnitureNote.tsx
+++ react/FurnitureNote.tsx
@@ -13,28 +13,31 @@ const CSS_HANDLES = [
   'noteTitle',
 ]
 
-const deliveryMessage = `
-  we’ll call the recipient in the next few days to arrange the furniture
-  delivery.
-  <br/>
-  <br/>
-  Please ensure sufficient space to receive the goods and keep in
-  mind that the couriers can’t hoist goods onto balconies.
-  <br/>
-`
+const DeliveryMessage = () => (
+  <>
+    we’ll call the recipient in the next few days to arrange the furniture delivery.
+    <br />
+    <br />
+    Please ensure sufficient space to receive the goods and keep in mind that the couriers can’t hoist goods onto balconies.
+    <br />
+  </>
+)
 
-const collectMessage = `
-  We’ll call the recipient to book a collection time with the warehouse.
-`
+const CollectMessage = () => (
+  <>
+    We’ll call the recipient to book a collection time with the warehouse.
+  </>
+)
 
 export const FurnitureNote = ({ shippingMethod }: FurnitureNoteProps) => {
   const handles = useCssHandles(CSS_HANDLES)
 
   return (
-    <div className={`${handles.furnitureNote}`}>
-      <h4 className={`${handles.noteTitle}`}>Furniture delivery</h4>
-      {/* using dangerouslySetInnerHTML because the <br/> tags were not being added previously */}
-      <p dangerouslySetInnerHTML={{ __html: shippingMethod === 'deliver' ? deliveryMessage : collectMessage }} />
+    <div className={handles.furnitureNote}>
+      <h4 className={handles.noteTitle}>Furniture delivery</h4>
+      <p>
+        {shippingMethod === 'deliver' ? <DeliveryMessage /> : <CollectMessage />}
+      </p>
       <a href="https://help.bash.com/support/solutions" className={handles.noteLink}>
         Frequently asked questions
       </a>
