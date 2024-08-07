1c378e77d289f6046ed445fa1d56f8f8d0b8e27f
Segopotso Makhutja <leketi.s@gmail.com>
2024-08-07 09:26:36 +0200

    add continue shopping button

--- react/ConfirmationMessage.tsx
+++ react/ConfirmationMessage.tsx
@@ -1,16 +1,28 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 
 import { useOrderGroup } from './components/OrderGroupContext'
 import { FurnitureNote } from './FurnitureNote'
-import { hasFurnitureDelivery } from './utils/functions'
+import { getCookie, hasFurnitureDelivery } from './utils/functions'
 
-const CSS_HANDLES = ['confirmationMessage']
+const CSS_HANDLES = ['confirmationMessage', 'continueShoppingButton', 'confirmationContainer']
 
 const ConfirmationMessage: FC = () => {
   const handles = useCssHandles(CSS_HANDLES)
   const orderGroup = useOrderGroup()
+  const [isApp, setIsApp] = useState(false)
+
+  useEffect(() => {
+    const isAppCookie = getCookie('is_app')
+    setIsApp(isAppCookie === 'true')
+  }, [])
+
+  const handleClick = () => {
+    if (typeof window !== 'undefined') {
+      window.location.replace('https://bash.com')
+    }
+  }
 
   const shippingMethod =
     orderGroup.orders[0].pickUpParcels.length > 0 ? 'collect' : 'deliver'
@@ -19,37 +31,43 @@ const ConfirmationMessage: FC = () => {
 
   return (
     <div className={`${handles.confirmationContainer}`}>
-      <p
-        className={`${handles.confirmationMessage} mt5 t-body tc c-muted-1 lh-copy`}
-      >
-        {orderGroup.orders[0].clientProfileData.customerEmail ? (
-          <FormattedMessage
-            id="store/header.email"
-            values={{
-              lineBreak: <br />,
-              userEmail: (
-                <strong className="nowrap">
-                  {orderGroup.orders[0].clientProfileData.customerEmail}
-                </strong>
-              ),
-            }}
-          />
-        ) : (
-          <FormattedMessage
-            id="store/header.phone"
-            values={{
-              lineBreak: <br />,
-              phone: (
-                <strong className="nowrap">
-                  {orderGroup.orders[0].clientProfileData.phone}
-                </strong>
-              ),
-            }}
-          />
+        <p
+          className={`${handles.confirmationMessage} mt5 t-body tc c-muted-1 lh-copy`}
+        >
+          {orderGroup.orders[0].clientProfileData.customerEmail ? (
+            <FormattedMessage
+              id="store/header.email"
+              values={{
+                lineBreak: <br />,
+                userEmail: (
+                  <strong className="nowrap">
+                    {orderGroup.orders[0].clientProfileData.customerEmail}
+                  </strong>
+                ),
+              }}
+            />
+          ) : (
+            <FormattedMessage
+              id="store/header.phone"
+              values={{
+                lineBreak: <br />,
+                phone: (
+                  <strong className="nowrap">
+                    {orderGroup.orders[0].clientProfileData.phone}
+                  </strong>
+                ),
+              }}
+            />
+          )}
+        </p>
+        {isApp && (
+          <button className={handles.continueShoppingButton} onClick={handleClick}>
+            Continue shopping
+          </button>
         )}
-      </p>
       {hasFurniture && shippingMethod !== 'collect' ? <FurnitureNote shippingMethod={shippingMethod} /> : null}
     </div>
   )
 }
+
 export default ConfirmationMessage
--- react/utils/functions.ts
+++ react/utils/functions.ts
@@ -36,3 +36,15 @@ export function hasFurnitureDelivery(orderGroup: OrderGroup): boolean {
 
   return false;
 }
+
+export function getCookie(name: string): string | undefined {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    const cookieValue = parts.pop();
+    if (cookieValue) {
+      return cookieValue.split(';').shift();
+    }
+  }
+  return undefined;
+}
