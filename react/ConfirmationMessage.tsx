d8af4127ddf10dd6d0dfa8618a463de3d375ad33
Alex Efremov <efremov.sakha@gmail.com>
2024-08-09 15:49:03 +0700

    Add isLoggedIn state to show a proper message

--- react/ConfirmationMessage.tsx
+++ react/ConfirmationMessage.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 
 import { useOrderGroup } from './components/OrderGroupContext'
+import { useSessionResponse } from './utils/useSessionResponse'
 
 const CSS_HANDLES = ['confirmationMessage']
 
@@ -10,18 +11,34 @@ const ConfirmationMessage: FC = () => {
   const handles = useCssHandles(CSS_HANDLES)
   const orderGroup = useOrderGroup()
   const profile = orderGroup.orders[0].clientProfileData
+  const sessionResponse = useSessionResponse()
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
+
+  useEffect(() => {
+    setIsLoggedIn(!!sessionResponse?.namespaces?.profile?.email)
+  }, [sessionResponse, setIsLoggedIn])
 
   return (
     <p
       className={`${handles.confirmationMessage} mt5 t-body tc c-muted-1 lh-copy`}
     >
-      <FormattedMessage
-        id="store/header.email"
-        values={{
-          lineBreak: <br />,
-          userEmail: <strong className="nowrap">{profile.email}</strong>,
-        }}
-      />
+      {isLoggedIn ? (
+        <FormattedMessage
+          id="store/header.email"
+          values={{
+            lineBreak: <br />,
+            userEmail: <strong className="nowrap">{profile.email}</strong>,
+          }}
+        />
+      ) : (
+        <FormattedMessage
+          id="store/header.guest-email"
+          values={{
+            lineBreak: <br />,
+            userEmail: <strong className="nowrap">{profile.email}</strong>,
+          }}
+        />
+      )}
     </p>
   )
 }
