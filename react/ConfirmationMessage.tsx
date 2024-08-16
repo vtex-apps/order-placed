3fc67e1c6ae18b836c1fa70c5f91e68f124522e9
Nosipho <manyasenosipho@gmail.com>
2024-08-16 04:36:28 +0200

    add app links for continue shopping btn

--- react/ConfirmationMessage.tsx
+++ react/ConfirmationMessage.tsx
@@ -5,8 +5,13 @@ import { useCssHandles } from 'vtex.css-handles'
 import { useOrderGroup } from './components/OrderGroupContext'
 import { FurnitureNote } from './FurnitureNote'
 import { getCookie, hasFurnitureDelivery } from './utils/functions'
+import { appHomeProd, appHomeStaging } from './utils'
 
-const CSS_HANDLES = ['confirmationMessage', 'continueShoppingButton', 'confirmationContainer']
+const CSS_HANDLES = [
+  'confirmationMessage',
+  'continueShoppingButton',
+  'confirmationContainer',
+]
 
 const ConfirmationMessage: FC = () => {
   const handles = useCssHandles(CSS_HANDLES)
@@ -19,9 +24,19 @@ const ConfirmationMessage: FC = () => {
   }, [])
 
   const handleClick = () => {
-    if (typeof window !== 'undefined') {
-      window.location.replace('com.tfglabs.manhattan.beta://auth')
+    if (typeof window === 'undefined') {
+      return
     }
+    let redirectUrl
+    if (isApp) {
+      redirectUrl = window.location.hostname.includes('staging')
+        ? appHomeStaging
+        : appHomeProd
+    } else {
+      redirectUrl = 'https://bash.com'
+    }
+
+    window.location.replace(redirectUrl)
   }
 
   const shippingMethod =
@@ -31,41 +46,46 @@ const ConfirmationMessage: FC = () => {
 
   return (
     <div className={`${handles.confirmationContainer}`}>
-        <p
-          className={`${handles.confirmationMessage} mt5 t-body tc c-muted-1 lh-copy`}
-        >
-          {orderGroup.orders[0].clientProfileData.customerEmail ? (
-            <FormattedMessage
-              id="store/header.email"
-              values={{
-                lineBreak: <br />,
-                userEmail: (
-                  <strong className="nowrap">
-                    {orderGroup.orders[0].clientProfileData.customerEmail}
-                  </strong>
-                ),
-              }}
-            />
-          ) : (
-            <FormattedMessage
-              id="store/header.phone"
-              values={{
-                lineBreak: <br />,
-                phone: (
-                  <strong className="nowrap">
-                    {orderGroup.orders[0].clientProfileData.phone}
-                  </strong>
-                ),
-              }}
-            />
-          )}
-        </p>
-        {isApp && (
-          <button className={handles.continueShoppingButton} onClick={handleClick}>
-            Continue shopping
-          </button>
+      <p
+        className={`${handles.confirmationMessage} mt5 t-body tc c-muted-1 lh-copy`}
+      >
+        {orderGroup.orders[0].clientProfileData.customerEmail ? (
+          <FormattedMessage
+            id="store/header.email"
+            values={{
+              lineBreak: <br />,
+              userEmail: (
+                <strong className="nowrap">
+                  {orderGroup.orders[0].clientProfileData.customerEmail}
+                </strong>
+              ),
+            }}
+          />
+        ) : (
+          <FormattedMessage
+            id="store/header.phone"
+            values={{
+              lineBreak: <br />,
+              phone: (
+                <strong className="nowrap">
+                  {orderGroup.orders[0].clientProfileData.phone}
+                </strong>
+              ),
+            }}
+          />
         )}
-      {hasFurniture && shippingMethod !== 'collect' ? <FurnitureNote shippingMethod={shippingMethod} /> : null}
+      </p>
+      {isApp && (
+        <button
+          className={handles.continueShoppingButton}
+          onClick={handleClick}
+        >
+          Continue shopping
+        </button>
+      )}
+      {hasFurniture && shippingMethod !== 'collect' ? (
+        <FurnitureNote shippingMethod={shippingMethod} />
+      ) : null}
     </div>
   )
 }
--- react/utils/constants.ts
+++ react/utils/constants.ts
@@ -2,3 +2,5 @@ export const customPath = '/custom-api/'
 export const baseApi = `https://store-api.www.bash.com${customPath}`
 export const baseApiQA = `https://store-api.staging.tfglabs.dev${customPath}`
 export const accountName = 'thefoschini'
+export const appHomeProd = 'com.tfglabs.manhattan.beta://home'
+export const appHomeStaging = 'com.bash://home'
